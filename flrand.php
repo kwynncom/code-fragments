522baad44157289e062db2d57b21638ea539f4a7
Kwynn <buessresume@gmail.com>
2020-12-29 21:58:03 -0500

    give up attempt 1

--- flrand.php
+++ flrand.php
@@ -1,26 +1,22 @@
-<?php 
+<?php // seems to fail any way
 
-/* The test is the following in "pack" mode: php flrand.php | rngtest
- * The test appears to be a failure for now, but outputting in binary mode is very helpful.
- * I will probably keep trying   */
-
-$m10 = pow(10, 17); // I tried several values for this, trying to fill up a 64 bit unsigned int
+$m10 = pow(10, 20); 
 
 $s = '';
 
 if (1) 
-for ($i=0; $i < 50; $i++) {
+for ($i=0; $i < 5000; $i++) {
     $a[$i] = nanopk();
     if ($i === 0) continue;
     $rat = ($a[$i]['Uns'] - $a[$i - 1]['Uns']) / 
 	   ($a[$i]['tsc'] - $a[$i - 1]['tsc']) ;
     
-    if (1) {
+    if (0) {
 	$rp3 = $rat * 1000;
     
 	$r20 = $rp3 - round($rp3);
 	$r25 = $r20;
-    }
+    } else $r20 = $rat;
     
     $r30 = intval($r20 * $m10);
     
@@ -32,11 +28,29 @@ for ($i=0; $i < 50; $i++) {
 	$ignore = 20;
     }
     
-    if (0) echo($r30 . "\n");
+    $r40 = decbin($r30);
+    
+    if (1) {
+	$b10 = trim(substr($r40, 1, 32));
+	if (strlen($b10) !== 32) {
+	    $ignore = 3;
+	}
+	
+	$r50 = bindec($b10);
+	
+	if ($r50 === 0) {
+	    $ignore = 4;
+	}
+	
+	// echo($r50 . "\n");
+    }
+    else if (0) echo($r30 . "\n");
     else if (1) echo(number_format($r30) . ' ' . decbin($r30) . "\n");
-    else if (1) {
-	$p = pack('Q', $r30);
+    else if (1) echo(decbin($r30) . "\n");
+    
+    if (1) {
+	$p = pack('L', $r50);
 	$l = strlen($p);
-	for($j=0; $j < 8; $j++) echo($p[$j]);
+	for($j=0; $j < 4; $j++) echo($p[$j]);
     }
 }
