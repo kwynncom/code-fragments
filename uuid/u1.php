b796a99fdab68a4f90f19cc11b17b23347c225f1
Kwynn <buessresume@gmail.com>
2020-12-30 00:07:45 -0500

    improvements to uuid

--- uuid/u1.php
+++ uuid/u1.php
@@ -4,6 +4,9 @@ require_once('/opt/kwynn/kwutils.php');
 require_once_ifex('/opt/kwynn/boot/mid.php');
 
 class uuidcl {
+    
+    private $oprs = '';
+    
     public static function get() {
 	$o = new self();
     }
@@ -14,31 +17,44 @@ class uuidcl {
 	echo $this->raws;
     }
 
+    private function hpr10(&$s, $pr) {
+	$this->oprs .= $pr;
+	if (PHP_SAPI === 'cli') $s .= $pr;
+    }
     
     private function p10() {
 	$s = '';
 	$i = 0;
 	foreach($this->aa as $r) {
+	    $lpr = '';
 	    $s .= ++$i . ' *** ' . $r['note'] . "\n";
 	    $b = nanopk();
 	    $t = $r['f']();
 	    $e = nanopk();
 	    $s .= self::toString($t);
 	    $d1 = $e['Uns'] - $b['Uns'];
-	    $s .= number_format($d1) . 'ns';
-	    $s .= ' / ';
-	    $d2 = $e['tsc'] - $b['tsc'];
-	    $s .= number_format($d2) . ' CPU ticks = ';
-	    $s .= ' ';
+	    $d2   = $e['tsc'] - $b['tsc'];
+	    $rat  = $d1 / $d2;
+	    $s   .= 'diff ns / diff CPU rounded = ' . round($rat, 1) . "\n";
 	    
-	    $rat = $d1 / $d2;
+	    $lpr .= number_format($e['tsc']) . "\n";
+	    $lpr .= number_format($b['tsc']) . "\n";
 	    
-	    $s .= sprintf('%0.30f', $rat); // attacker might infer boot time from clock drift *** !!!!! **** - so make this private / CLI but part of hash
+	    $nfd2 =  number_format($d2);
 	    
+	    $lpr .= sprintf('%18s', $nfd2) . "\n";
+    
+	    $nfd1 =  number_format($d1);
+	    
+//	    $lpr .= sprintf('%0.30f', $rat); // attacker might infer boot time from clock drift *** !!!!! **** - so make this private / CLI but part of hash
+	    
+	    $s .= number_format($e['Uns']) . "\n";
+	    $s .= number_format($b['Uns']) . "\n";
+	    $s .= sprintf('%25s', $nfd1) . "\n";
+	    $s .= 'core ' . $b['pid'] . ' / ' . $e['pid'];
 	    $s .= "\n";
-	    $s .= number_format($e['Uns']) . ' - ' . number_format($b['Uns']) . ' core ' . $b['pid'] . ' then ' . $e['pid'];
 	    
-	    $s .= "\n";
+	    $this->hpr10($s, $lpr);
 	}
 	
 	$this->raws = $s;
@@ -55,26 +71,37 @@ class uuidcl {
 
     }
     
+    public static function time() { 
+	return number_format(time());
+    }
+    
     private function d10() {
 	$a = [
-	    ['note' => 'MongoDB OID original and human-readable', 'f' => ['uuidcl', 'oid'], 'cl' => 'pub'],
-	    ['note' => 'x86 TSC, PID - from my PHP extension', 'f' => 'rdtscp', 'cl' => 'cli'],
-	    ['note' => 'nanotime - from my PHP extension', 'f' => 'nanotime', 'cl' => 'pub'],
-	    ['note' => 'time, seconds', 'f' => 'time', 'cl' => 'pub'],
-	    ['note' => 'process ID', 'f' => 'getmypid', 'cl' => 'cli'],
-	    ['note' => 'machine ID stuff, my own', 'f' => ['uuidcl', 'mid'], 'cl' => 'cli'],
-	    ['note' => 'rough-yet-absurdly precise machine locations' , 'f' => ['uuidcl', 'ec2_phy'], 'cl' => 'pub'],
-	    ['note' => 'base62, in kwutils.php' , 'f' => 'base62', 'cl' => 'pub'],
-	    ['note' => 'random_int()', 'f' => ['uuidcl', 'rint'], 'cl' => 'pub'],
-	    ['note' => 'more random',  'f' => ['uuidcl', 'randrand'], 'cl' => 'pub'],	    
+	    ['note' => 'MongoDB OID original and human-readable', 'f' => ['uuidcl', 'oid']],
+	    ['note' => 'time, seconds', 'f' => ['uuidcl', 'time']],
+	    ['note' => 'process ID', 'f' => [$this, 'pid']],
+	    ['note' => 'machine ID stuff, my own', 'f' => ['uuidcl', 'mid']],
+	    ['note' => 'rough-yet-absurdly precise machine locations' , 'f' => ['uuidcl', 'ec2_phy']],
+	    ['note' => 'base62, in kwutils.php' , 'f' => 'base62'],
+	    ['note' => 'random_int()', 'f' => ['uuidcl', 'rint']],
+	    ['note' => 'more random',  'f' => ['uuidcl', 'randrand']],	    
 	    ];
 	
 	$this->aa = $a;
     }
     
+    private function pid() {
+	$pid = getmypid();
+	$aud = 'process ID is integer and >= 1, included in string';
+	kwas(is_integer($pid) && $pid >= 1, $aud);
+	$this->oprs .= 'process id = ' . $pid . "\n";
+	return $aud;
+    }
+    
     public static function randrand($csz = 65) {
 	$l = random_int(70, 500);
-	$s = base62($l);
+	// $s = base62($l);
+	$s = base64_encode(random_bytes($l)); // much, much faster than my base62()
 	$i = 0;
 	$r = '';
 	do {
@@ -93,7 +120,12 @@ class uuidcl {
     public static function mid() {
 	$c = ['machine_id', 'get'];
 	if (!(class_exists($c[0]))) return '';
-	return $c();
+	$a = $c();
+	$s = '';
+	$s .= 'machine ID hash of hash: ' . "\n" . $a['hash_of_hash'] . "\n";
+	$s .= 'machine ID # of private fields hashed = ' . $a['private_field_count'] . "\n";
+	$s .= 'machine ID ' . $a['midv'];
+	return $s;
     }
     
     public static function ec2_phy() {
