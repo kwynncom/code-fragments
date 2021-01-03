4694c331cced9d8d38b1c2ec13d2629b229dc2b9
Kwynn <buessresume@gmail.com>
2021-01-02 22:13:09 -0500

    nanopk() triplets x2

--- ticks/triplets.php
+++ ticks/triplets.php
@@ -1,16 +1,29 @@
 <?php
 
+$version = 'blah 2 asfasfasfsdf';
+
+$iter = 20;
 $i = 0;
-
-$outr = fopen('/dev/null', 'w');
-
 $r = [];
+$s = [];
+$v = $d = PHP_INT_MAX;
+$min = PHP_INT_MAX;
+$com = [];
+$ret = [];
+
 do {
     for($j=0; $j < 3    ; $j++) $r[$j] = nanopk();
     for($j=0; $j < 3 - 1; $j++) $s[$j] = $r[$j+1]['Uns'] - $r[$j]['Uns'];
     sort($s);
-    $d = $s[1];
-    fwrite($outr, $d . "\n");
-    if ($i < 4) continue;
-    $outr = STDOUT;
-} while($i++ < 40);
+    $v = $s[1];
+    $com['pk']  = $r;
+    $com['maxd'] = $v;
+    $com['iter'] = $iter;
+    $com['Uns' ] = $r[1]['Uns'];
+    if ($v < $min) $ret = $com;
+    $d = $v . "\n";
+    if ($i < 2) continue;
+//    echo($d);
+} while($i++ < $iter);
+
+var_dump($com);
