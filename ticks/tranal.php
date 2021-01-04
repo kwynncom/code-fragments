8e123cf0e96b14ca7e55abe18b73470747c35bc7
Kwynn <buessresume@gmail.com>
2021-01-03 22:49:46 -0500

    more TSC experiments

--- ticks/tranal.php
+++ ticks/tranal.php
@@ -2,6 +2,7 @@
 
 require_once('storeTicks.php');
 require_once('triplets.php');
+require_once('quick.php');
 
 class triplet_anal extends ticks_tracker {
     
@@ -13,7 +14,20 @@ class triplet_anal extends ticks_tracker {
 	$this->p10();
 	}
 	
-	$this->p20();
+	// $this->p20();
+	$this->p30();
+    }
+    
+    private function p30() {
+	for($i=0; $i < 100000; $i++) {
+	    $r = getStableNanoPK();
+	    if ($i === 0) { $p = $r;  continue;    }
+	    $rat = tick_time_study::rat($r, $p);
+	    echo(sprintf('%0.14f', $rat) . "\n");
+	    $p = $r;
+	    usleep(100000);
+	    
+	}
     }
     
     private function p20() {
