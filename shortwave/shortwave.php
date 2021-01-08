4d30a70a571609cbc6acfc559fbe380b3f82bd18
Kwynn <buessresume@gmail.com>
2021-01-07 23:23:32 -0500

    back to shortwave, about to try log again

--- shortwave/shortwave.php
+++ shortwave/shortwave.php
@@ -1,29 +1,33 @@
 <?php
 
 require_once('/opt/kwynn/kwutils.php');
-require_once(__DIR__ . '/../ticks/stddev.php');
+require_once(__DIR__ . '/../ticks/batch1/stddev.php');
 
-$file = '/tmp/hwrset1.wav';
+$file = '/tmp/r1.wav';
 
-$bytesPerSam = 2;
+$bytesPerSam = 4;
 $channels = 2;
-$sampleRate = 8000;
+$sampleRate = 48000;
 $bitsPerSam = $bytesPerSam * 8;
-$duration = 80;
+$duration = 90;
+
+if      ($bytesPerSam === 4) $packf = 'l';
+else if ($bytesPerSam === 2) $packf = 'S';
+
 $cmd = 'arecord -f S' . $bitsPerSam . '_LE -c ' . $channels . ' -r ' . $sampleRate . ' --device="hw:0,0" -d ' . $duration . ' > ' . $file;
 
 // arecord -f S16_LE -c 2 -r 8000 --device="hw:0,0" -d 2 > /tmp/hwrset1.wav
-if (1) {
+if (0) {
 kwas(unlink($file), 'delete failed');
 exec($cmd);
 }
 
 $wsig = file_get_contents($file);
 $l = strlen($wsig);
-$bps = $channels * $bytesPerSam * $sampleRate;
+$bpsec = $channels * $bytesPerSam * $sampleRate;
 $header = 44;
 
-kwas($l === $bps * $duration + $header, 'bad length');
+kwas($l === $bpsec * $duration + $header, 'bad length');
 
 $wsig = substr($wsig, 44); // get rid of header
 $l -= 44;
@@ -32,93 +36,59 @@ $min = $minl = PHP_INT_MAX;
 $sto = false;
 
 $soffhun = 5;
-$sofbytes = $soffhun * intval(round(($bps / 100))) * $channels * $bytesPerSam;
+$spp60kHz = 0.000016667;
+$peroff = 0.1;
+$sofbytesraw = intval(round(($spp60kHz * $peroff + ($soffhun * ($bpsec / 100))) * $channels * $bytesPerSam));
+
+for($i=0; $i < $bytesPerSam; $i++) 
+    if (($sofbytesraw + $i) % $bytesPerSam === 0) { $sofbytes = $sofbytesraw + $i; break;} 
 
 $ec = 0;
 
-for($i=($bps * 10) + $sofbytes; $i < $l - 8; $i += $channels * $bytesPerSam) {
+$bus = [];
+
+for($i=($bpsec * 30) + $sofbytes; $i < $l - 8; $i += (($channels * $bytesPerSam) * 5)) {
     
-    $sec = ($i / $bps);
+    $sec = ($i / $bpsec);
     $ti = intval(floor($sec * 10));
     
-    for($j=0; $j < 2; $j++) {
-	$byte = $i + $j * 2;
-	$subs = substr($wsig, $byte, 2);
-	$u = unpack('s', $subs);
+    for($j=0; $j < $channels; $j++) {
+	$byte = $i + $j * $bytesPerSam;
+	$subs = substr($wsig, $byte, $bytesPerSam);
+	$u = unpack($packf, $subs);
 	$isigraw = $u[1];
 	$is10 = $isigraw < 0 ? -$isigraw : $isigraw;
 	// echo($j . ' ' . $is10 . "\n");
 	if ($j === 0) $j0 = $is10;
 	if ($j === 1) $j1 = $is10;
-//	doStats($sto, $ti, $is10);
+	// doStats($sto, $ti, $is10);
    }
    
-   filterStats($sto, $ti, $j0, $j1);
+   if (!isset($bus[$ti])) $bus[$ti] = 0;
+   $bus[$ti] += $j0 + $j1;
    
-    if (0) {echo(abs($j0 - $j1) . ' ' . $j1 . ' ' . $j0 . "\n");
-    if ($ec++ > 8000) exit(0); }
-    
     
 }
 
-function filterStats(&$stin, $i, $d0, $d1) {
-    // if ($d0 >= 30 && $d1 >= 30) $d = 1;
-    // else $d = 0;
-    $sum = $d0 + $d1;
-    if ($sum >= 71) $d = 1;
-    else $d = 0;
-    
-    doStats($stin, $i, $d);
-    
-}
+$sdo = new stddev();
 
-foreach($sto as $i => $o) {
-    
-    $a = $o->get();
-    
-    // var_dump($a); continue;
-    $s  = '';    
-    $av = $a['a'];
-    
-    if (0) {if ($av > 0.60) $s .= 1;
-    else $s .= 0;}
-    else if (1) $s .= sprintf('%0.5f', $av);
-    else $s .= round($av);
-    // $ad = number_format(round($av));
+foreach($bus as $i => $v) $sdo->put($v);
+$sdr = $sdo->get();
+$av = $sdr['a'];
 
-    // $s .= $i . ' ';
-    // $s .= $ad;
-    // $s .= sprintf('%e', (round($av)));
-    // $s .= number_format($a['a']);
-    $s .= ' ';
+
+foreach($bus as $i => $v) {
     
-    // $s .= $i;
-    // $s .= ' ';
-    // $s .= $ad;
-    $s .= ' ';
-    // $s .= sprintf('%0.2f' , $a['s']);
-    $s .= "\n";
-    echo($s);
+    $d = $v - $av;
+    
+    if ($d > 0) echo 1;
+    else	echo 0;
+    echo ' ';
+    echo(sprintf('%010d', $d));
+    echo "\n";
+    
+    // echo(number_format($v - $av) . "\n");
     
 }
 
 exit(0);
-
-function doStats(&$stin, $iin, $din) {
-    
-    static $thisi = 0;
-    static $thiss = 0;
-   
-    // if ($din < 0.001) return;     // for filtering weird logs //  
-    if (!isset($stin[$iin])) $stin[$iin] = new stddev();
-    if (0 && isset(   $stin[$iin - 1])) {
-	$a = $stin[$iin - 1]->get();
-	$thiss += $a['s'];
-	if ($thisi++ === 1000) {
-	    echo(number_format($thiss) . "\n");
-	    exit(0);
-	}
-	
-    }
-    $stin[$iin]->put($din);
-}
