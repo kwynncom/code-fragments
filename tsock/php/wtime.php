5a9fc1fb0df9ea66f1d63264ba5c6a33be9afb8c
Kwynn <buessresume@gmail.com>
2021-01-31 20:56:21 -0500

    and now with a keep-alive TCP connection

--- tsock/php/wtime.php
+++ tsock/php/wtime.php
@@ -1,16 +1,35 @@
 <?php
 
+require_once('/opt/kwynn/kwutils.php');
+
+
 for($i=0; $i < 20; $i++) nanotime();
+
+$sr = socket_create(AF_INET, SOCK_STREAM, 6);
+kwas(socket_connect($sr, 'kwynn.com', 80), 's conn failed - 620');
+
+$req = '';
+$req .= 'GET /t/20/10/timeserver/nstimeraw.php' . ' HTTP/1.1' . "\r\n";
+$req .= 'Host: kwynn.com' . "\r\n";
+$req .= 'Connection: keep-alive' . "\r\n";
+$req .= "\r\n";
 
 for($i=0; $i < 20; $i++) {
 $b   = nanotime();
-$stis = file_get_contents('http://[2600:1f18:23ab:9500:acc1:69c5:2674:8c03]/t/20/10/timeserver/nstimeraw.php');
+socket_write($sr, $req);
+$htr = socket_read($sr, 2000);
 $e = nanotime();
-$sti = (int)$stis;
+$sti = getTime($htr); unset($htr);
 $avg = ($b + $e) >> 1;
 $d = $sti - $avg;
 $dd = number_format($d);
 $nd = $e - $b;
 $ndd = number_format($nd);
 echo($dd . ' ' . $ndd . "\n");
+}
+
+function getTime($din) {
+   $pts = explode("\r\n\r\n", $din);
+   return (int)trim($pts[1]);
+    
 }
