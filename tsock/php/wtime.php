633c56bed4c0185d423d8f1e1ccc72ab6296f1cb
Kwynn <buessresume@gmail.com>
2021-02-01 22:48:17 -0500

    web keep alive versus not

--- tsock/php/wtime.php
+++ tsock/php/wtime.php
@@ -2,6 +2,8 @@
 
 require_once('/opt/kwynn/kwutils.php');
 
+$keepa = true;
+
 if (0 || isAWS()) {
     $server = 'kwynn.com';
     $url    = '/t/20/10/timeserver/nstimeraw.php';
@@ -11,23 +13,28 @@ else {
     $url    = '/tsnano/nstimeraw.php';
 }
 
-for($i=0; $i < 8; $i++) nanotime();
-$sr = socket_create(AF_INET, SOCK_STREAM, 6);
-kwas(socket_connect($sr, $server, 80), 's conn failed - 620');
+if ($keepa) {
+    $sr = socket_create(AF_INET, SOCK_STREAM, 6);
+    kwas(socket_connect($sr, $server, 80), 's conn failed - 620');
+    $req = '';
+    $req .= 'GET ' . $url . ' HTTP/1.1' . "\r\n";
+    $req .= 'Host: ' .  $server . "\r\n";
+    $req .= 'Connection: keep-alive' . "\r\n";
+    $req .= "\r\n";
+} else
+    $req = 'http://' . $server . '/' . $url;
 
-$req = '';
-$req .= 'GET ' . $url . ' HTTP/1.1' . "\r\n";
-$req .= 'Host: ' .  $server . "\r\n";
-$req .= 'Connection: keep-alive' . "\r\n";
-$req .= "\r\n";
+for($i=0; $i < 8; $i++) nanotime();
 
 for($i=0; $i < 20; $i++) {
-$b   = nanotime();
-socket_write($sr, $req);
-$htr = socket_read($sr, 2000);
-$e = nanotime();
-$r = getTime($htr);
-echo("$b\n$r\n$e\n");
+    $b   = nanotime();
+    if ($keepa) {
+	socket_write($sr, $req);
+	$htr = socket_read($sr, 2000);
+    } else $r = file_get_contents($req);
+    $e = nanotime();
+    if ($keepa) $r = getTime($htr);
+    echo("$b\n$r\n$e\n");
 }
 
 function getTime($din) {
