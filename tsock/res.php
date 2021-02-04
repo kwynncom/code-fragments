97f8a78651e29313997e755d4faba6bc3c8d4684
Kwynn <buessresume@gmail.com>
2021-02-04 17:50:13 -0500

    C sntp client loop

--- tsock/res.php
+++ tsock/res.php
@@ -6,10 +6,13 @@
 require_once('/opt/kwynn/kwutils.php');
 require_once(__DIR__ . '/../sntp/sntp.php');
 
+$exl = 64; // expected length
+
 $f = "php://stdin";
 // $f = './tf1.bin';
-$r = file_get_contents($f);
-kwas(strlen($r) === 64, 'bad result len - sntp raw C version 112');
+while ($r = fread(STDIN, $exl)) { 
+// $r = file_get_contents($f);
+kwas(strlen($r) === $exl, 'bad result len - sntp raw C version 112');
 $pa = unpack('q2', $r);
 $snp = substr($r, PHP_INT_SIZE * 2);
 $sa = sntp_get_actual::parsePacket($snp);
@@ -17,4 +20,5 @@ $sa = sntp_get_actual::parsePacket($snp);
 if (!isset($pa[1]) || !isset($pa[2]) || !isset($sa['avg'])) die('not 3 fields C - PHP SNTP 155');
 
 echo("$pa[1]\n$sa[avg]\n$pa[2]\n");
+}
 exit(0);
--- tsock/s.c
+++ tsock/s.c
@@ -15,30 +15,29 @@ void getPacket(char (*sntpp)[SNTP_PLEN]);
 void getPacket(char (*sntpp)[SNTP_PLEN]) {
     FILE *fp;
 
-    if (fread(sntpp, SNTP_PLEN, 1, stdin) != 1) { printf("sntp client package bad size - stdin edition"); exit(1); }    
-
-    if (0) {
+    if (1) {
     fp = popen("php ./cp.php", "r");
     if (fp == NULL) { printf("Failed to run command\n" ); exit(1);   }
     if (fread(sntpp, SNTP_PLEN, 1, fp) != 1) { printf("sntp client package bad size"); exit(1); }
     pclose(fp);
-    }
+    } else if (fread(sntpp, SNTP_PLEN, 1, stdin) != 1) { printf("sntp client package bad size - stdin edition"); exit(1); }    
 }
 
 void main() {
     char pack[SNTP_PLEN];
-    getPacket(&pack);
     int writer;
+    long b, e;
+    int sock = getBoundSock(0, "34.193.238.16", 123); // 0 means UDP, which it must be for NTP
 
-    int sock = getBoundSock(0, "34.193.238.16", 123);
-    long b = nanotime();
+    for (int i=0; i < 20; i++) {
+    getPacket(&pack);
+    b = nanotime();
     if (write(sock, pack, SNTP_PLEN) != SNTP_PLEN) { printf("bad write size"); exit(1);}
     if (read (sock, pack, SNTP_PLEN) != SNTP_PLEN) { printf("bad read size"); exit(1);}
-    long e = nanotime();
-
-    char outs[SNTP_PLEN + sizeof(b) + sizeof(e)];
+    e = nanotime();
 
     fwrite(&b, sizeof(b), 1, stdout);
     fwrite(&e, sizeof(e), 1, stdout);
     fwrite(&pack, sizeof(pack), 1, stdout);
+    }
 }
