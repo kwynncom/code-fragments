41ede5d397999551e92d6792667200459312518a
Kwynn <buessresume@gmail.com>
2021-02-14 20:28:34 -0500

    bug version of Mongo to HTML or not complete

---
+++ atlas/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>MongoDB to HTML</title>
+
+<script>
+    
+    <!-- 2021/02/14 8:28pm - bug version / or incomplete -->
+    
+    var INIT_GVAR = <?php echo(json_decode($PEOPLE_GLOBAL)); ?>;
+    window.onload = function() {
+	INIT_GVAR.forEach(function()	);
+    }
+</script>
+</head>
+<body>
+    <table>
+	<thead>
+	    <tr><th>name</th><th>age</th><th>species</th></tr>
+	</thead>
+	<tbody id='outTable10'>
+	</tbody>
+    </table>
+    
+</body>
+</html>
--- atlas/readPeople.php
+++ atlas/readPeople.php
@@ -6,5 +6,7 @@ require_once('/opt/kwynn/kwutils.php');
 
 $cli = new MongoDB\Client(file_get_contents('/var/mongo_ad1_2021_1.txt'), [], ['typeMap' => ['array' => 'array','document' => 'array', 'root' => 'array']]);
 $coll1  = $cli->selectCollection('kwynnTest', 'kwynnTestInsert');
-$res = $coll1->find()->toArray(); // this is an array and thus legible when viewed in debugger or var_dump'ed
-var_dump($res);
+
+$PEOPLE_GLOBAL = $coll1->find()->toArray();
+
+unset($cli, $coll1); // because global and not needed anymore; keep global space clean
