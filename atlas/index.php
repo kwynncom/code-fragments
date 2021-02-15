d50bafe85677c88b11e9d1b227aea2147dc7a19e
Kwynn <buessresume@gmail.com>
2021-02-14 20:46:39 -0500

    db to HTML in full - init at least

--- atlas/index.php
+++ atlas/index.php
@@ -7,18 +7,32 @@
 <title>MongoDB to HTML</title>
 
 <script>
+    
+    function byid(id) { return document.getElementById(id); }
+    function cree(ty) { return document.createElement (ty); }
+    
+    
     <?php require_once('readPeople.php'); ?>
     
     var INIT_GVAR = <?php echo(json_encode($PEOPLE_GLOBAL)); ?>;
     window.onload = function() {
-	INIT_GVAR.forEach(function() {}	);
+	INIT_GVAR.forEach(function(row) {
+	    const tr = cree('tr');
+	    for(const [key, value] of Object.entries(row)) {
+		if (key !== 'age' && key !== 'species' && key !== 'name') continue;
+		const td = cree('td');
+		td.innerHTML = value;
+		tr.append(td);
+	    }
+	    byid('outTable10').append(tr);
+	});
     }
 </script>
 </head>
 <body>
     <table>
 	<thead>
-	    <tr><th>name</th><th>age</th><th>species</th></tr>
+	    <tr><th>name</th><th>species</th><th>age</th></tr>
 	</thead>
 	<tbody id='outTable10'>
 	</tbody>
