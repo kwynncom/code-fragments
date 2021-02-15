e7df512278f5701fff436c60d11148db8da5716f
Kwynn <buessresume@gmail.com>
2021-02-14 20:19:09 -0500

    find()->toArray()

--- atlas/readPeople.php
+++ atlas/readPeople.php
@@ -6,5 +6,5 @@ require_once('/opt/kwynn/kwutils.php');
 
 $cli = new MongoDB\Client(file_get_contents('/var/mongo_ad1_2021_1.txt'), [], ['typeMap' => ['array' => 'array','document' => 'array', 'root' => 'array']]);
 $coll1  = $cli->selectCollection('kwynnTest', 'kwynnTestInsert');
-$res = $coll1->find(); // note that this is NOT an array.  Look what it gives you
+$res = $coll1->find()->toArray(); // this is an array and thus legible when viewed in debugger or var_dump'ed
 var_dump($res);
