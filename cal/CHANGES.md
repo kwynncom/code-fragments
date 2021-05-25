a6183664e32a776e5feb6cf1741e8d129ce27508
Kwynn <buessresume@gmail.com>
2021-05-25 01:03:37 -0400

    PHP removed from calendar

---
+++ cal/CHANGES.md
@@ -0,0 +1,7 @@
+2021/05/25 1:01am EDT / UTC -4 / New York / Atlanta / Eastern Daylight Time
+
+JavaScript only - PHP removed
+
+2021/05/24 - end of the day
+
+Working with PHP functions for days in month, name of month, day of week of first day of month
--- cal/js.js
+++ cal/js.js
@@ -2,7 +2,7 @@ class kwCalendar {
     constructor(parid, h1id, init) {
 	this.pele = byid(parid);
 	this.h1e  = byid(h1id);
-	this.inito = init;
+	this.inito = kwCalMonthInfo();
 	this.dayi = 0;
 	this.h1e.innerHTML = this.inito.monthh1;
 	this.f10();
@@ -29,4 +29,22 @@ class kwCalendar {
 
 	this.pele.append(de);
     }
+}
+
+function kwCalMonthInfo() {
+    const r = {};
+    const now = new Date();
+    const d1 = new Date(now.getFullYear(), now.getMonth(), 1);
+    
+    r.day1w = d1.getDay();
+    r.dinm  = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+    r.monthh1 = monthName(now.getMonth()) + ', ' + now.getFullYear();
+    return r;
+}
+
+function monthName(mjs0i) { // month JavaScript 0 index
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    return monthNames[mjs0i];
 }
