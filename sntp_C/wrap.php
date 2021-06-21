8fcfebf330c70a6d0b5155ea23f9e6a706c5c98b
Kwynn <buessresume@gmail.com>
2021-06-20 20:01:56 -0400

    sleep loop version inc but not as seconds

--- sntp_C/wrap.php
+++ sntp_C/wrap.php
@@ -10,44 +10,44 @@ $count = 1;
 $itlen = 48; //  + 8 + 8;
 $totlen = $itlen * $count;
 
+for($j=0; $j < 10; $j++) {
+for($i=0; $i < $count; $i++) {
 $r = popen('./sntp', 'r');
 $wr = fread($r, $totlen); kwas(strlen($wr) === $totlen, "sntp wrap fread not $totlen bytes");
 pclose($r); unset($r);
 
-for($i=0; $i < $count; $i++) {
-
-
-
-$s = substr($wr, $itlen * $i, $itlen);
 
  mytest(substr($wr, 0, 48));
-exit(0);
 
-$unpch = 'KwC1';
+}
 
-// sntp_get_actual::b8tonsC($s, 24, $unpch); // test outgoing
+sleep(1);
 
-$rr =  sntp_get_actual::b8tosf($s, 32, $ignore, 'blah', 'blah', 'N');
-$rr = sntp_get_actual::b8tonsC($s, 32, $unpch);
-$rs = sntp_get_actual::b8tonsC($s, 40, $unpch);
-$bb = substr($s, 48, 8);
-$ba  = unpack('Q', $bb);
-$ea = unpack('Q', substr($s, 56, 8));
-$b = $ba[1];
-$e = $ea[1];
-$vars = get_defined_vars();
-$fs = ['b', 'rr', 'rs', 'e'];
-foreach($fs as $f) echo(number_format($vars[$f]) . "\n");
-echo("\n");
 }
 exit(0);
 }
 
 function mytest($p) {
-	for($i=5; $i < 6; $i++) {
-		$lp = substr($p, 40, 8);
-		$upn = unpack('Q', $lp);
-		$d = $upn[1];
-		echo($d . "\n");
+	$lp = substr($p, 40, 8);
+	$upn = unpack('Q', $lp);
+	$d10 = $upn[1];
+
+	if (0) echo($upn[1] . "\n");
+
+	if(0) {
+	$d = $upn[1];
+	echo($upn[1] . ' ' . $upn[2] . "\n");		
 	}
+
+
+	if (1) {
+		$s10 = $d10 >> 32;
+		echo($s10 . "\n");
+
+		if (0) {$u20 = unpack('V', $s10);
+			$d20 = $u20[1];
+		echo(decbin($d20) . "\n");}
+
+	}
+
 }
