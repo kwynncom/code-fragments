38e159f79d3447721b4c8c9ce32bd7bfdbd91f9d
kwynncom <buessresume@gmail.com>
2021-07-06 00:35:17 -0400

    session experiments

---
+++ sid/session.php
@@ -0,0 +1,11 @@
+<?php
+
+// the default of 0 doesn't seem to go away easily upon browser restart
+
+// session_start(['cookie_lifetime' => 1]);
+// session_destroy();
+// setcookie("PHPSESSID", "", time() - 3600);
+
+exit(0);
+
+
