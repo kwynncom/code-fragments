38fb401961198a1aab5046578f700ff20aad59df
kwynncom <buessresume@gmail.com>
2021-07-11 22:43:33 -0400

    ping3 rc1

--- jsping3/ping10.js
+++ jsping3/ping10.js
@@ -17,3 +17,42 @@ class do1ping {
 		this.no.send();
 	}
 }
+
+class draw1Ping {
+	constructor(tbid, diob, groupn) {
+		this.tb = byid(tbid);
+		this.diob = diob;
+		this.groupn = groupn;
+		this.do10();
+	}
+
+	do10() {
+		const tr = cree('tr');
+		this.tb.prepend(tr);
+		const tdst = cree('td');
+		tdst.className = 'eldi';
+		const pos = [];
+		let i = 0;
+		const self = this;
+		for (i=0; i < this.groupn; i++) {
+			const td = cree('td');
+			tr.append(td);
+			pos[i] = new do1ping(function(d) {
+									self.diob.reg(d, tdst, td);
+									self.iter1();
+								 }
+			);
+		}
+		tr.append(tdst);
+		this.pos = pos;
+		this.iterInit();
+	}
+
+	iterInit() {
+		this.iteri = 0;
+		this.pos[this.iteri].ping();			
+	}
+
+	iter1() { if (this.pos[++this.iteri]) this.pos[this.iteri].ping(); }
+}
+	
