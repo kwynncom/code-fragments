28375bf0a8012d521f818bcc31403fedd5355b09
kwynncom <buessresume@gmail.com>
2021-10-21 02:11:25 -0400

    backoff no longer simulating but calcing

--- backoff.php
+++ backoff.php
@@ -12,91 +12,63 @@ class usebo {
 	}
 
 	private function init() {
-		$this->boo = new backoff();
-		$this->baset = nanotime(); // for reporting only
+		$this->boo = new backoff(4, 1200, 1.2);
+		$this->totd = 0;
 	}
 	
 	private function doit() {
-		for ($i=0; $i < 60; $i++) {
-			$r = $this->boo->okin();
-			if ($r === true) {  $this->douse(0); continue; }
-			kwas(is_integer($r) && $r > 0);
-			usleep($r);
-			$this->douse($r);
+		for ($i=0; $i < 50; $i++) {
+			$r = $this->boo->next();
+			$this->douse($i, $r);
 		}
-
 	}
 	
-	private function douse($delay) {
-		$this->usa[] = nanotime();	
-		$this->repit($delay);
+	private function douse($i, $delay) {
+		$this->rec[] = $delay;
+		$this->repit($i, $delay);
 	}
 	
-	private function repit($delay) {
-		$c = count($this->usa);
-		
-		if (!isset($this->usa[$c - 2])) return;
-		
-		$sm1 = $this->usa[$c - 1];
-		$sm2 = $this->usa[$c - 2];
-		
-		echo($c . ' ' . $delay . ' ' /* . number_format($sm1 - $sm2) */ . "\n");
+	private function repit($i, $delay) {
+		echo(($i + 1) . ' ' . $delay . ' ' . "\n");
+		$this->totd += $delay;
 	}
 	
 	private function repcon() {
-		$c = count($this->usa);
-		if ($c < 2) return;
-		$e = $this->usa[$c - 1] - $this->baset;
-		
-		$esf = $e / M_BILLION;
-		$esi = round($esf);
-		
-		echo($c . ' calls in ' . $esi .  ' s' . "\n");
-		
-		$rf = $c / $esf;
-		$ri = round($rf);
-
-		echo($ri . ' calls per s' . "\n");
-		
+		$iuf = count($this->rec) / $this->totd;
+		$sca = $iuf * 3600;
+		$iui = intval(round($sca));
+		echo($iui . ' iter per unit scaled' . "\n");
 	}
-	
 }
 
 
 class backoff {
 	
-	const mind = 4;
-	const maxd  = 1200;
-	const scale = 3600;
-
-	
-	public function __construct() {
+	public function __construct($mind, $maxd, $pow) {
+		$this->mind = $mind;
+		$this->maxd = $maxd;
+		$this->powv = $pow;
 		$this->cav = 0;
 		$this->cari = 0;
 	}
 	
-	public function okin() {
-		if ($this->cav++ === 0) return true;
-		$ac = $this->cav - $this->cari;
-		if ($ac <= 1) return true;
+	public function next() {
+		$ac = $this->cav++ - $this->cari;
 		$x = $this->x($ac);
 		$this->reset($x);
-		$scx = $x * self::scale;
-		return $scx;
+		return $x;
 	}
 	
 	private function x($n) {
-		$n = intval(round($n)) - 1; 
-		$n = intval(round(self::mind * pow(1.05, $n)));	
+		$n = intval(round($this->mind * pow($this->powv, $n)));	
 		$n = $this->limit($n);
-		if ($n < self::mind) return self::mind;
-		return $n;
+		return $n < $this->mind ? $this->mind : $n;
 	}
 	
-	private function limit($n) { return $n > self::maxd ? self::maxd : $n; }
+	private function limit($n) { return $n > $this->maxd ? $this->maxd : $n; }
 	
 	private function reset($x) {
-		if ($x < self::maxd) return;
+		if ($x < $this->maxd) return;
 		$this->cari = $this->cav;
 	}
 }
