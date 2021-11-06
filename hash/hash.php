20330937d54a6b005e02e3359026ce8dca1ff007
kwynncom <buessresume@gmail.com>
2021-11-06 00:14:00 -0400

    small changes

--- hash/hash.php
+++ hash/hash.php
@@ -9,27 +9,27 @@ function password_hash_test() {
 	
 	global $argv;
 	
-	$pwa = ['threads'     => $argv[1],
+	$phia = ['threads'     => $argv[1],
 			'memory_cost' => $argv[2] * 1000,
-			'time_cost'   => $argv[3]];
+			'time_cost'   => $argv[3]]; unset($argv);
 
 	
 	$pwd = base62(40);
 
 	$b	  = microtime(1);
-	$hash = password_hash($pwd, PASSWORD_ARGON2ID, $pwa); unset($pwa);
+	$hash = password_hash($pwd, PASSWORD_ARGON2ID, $phia); unset($phia);
 	$e	  = microtime(1);
 
 	$elapf = $e - $b; unset($e, $b);
 	$elaps = sprintf('%0.2f', $elapf);
-	$all['exec_seconds_string'] = $elaps; unset($elaps);
+	$all['exeSSt'] = $elaps; unset($elaps);
 	$piao  = password_get_info($hash);
 	$piaoo = $piao['options']; unset($piao);
-	$piaoo['memory_cost_mb'] = $piaoo['memory_cost'] / 1000;
+	$piaoo['mem_mb'] = $piaoo['memory_cost'] / 1000;
 	$all   = array_merge($all, $piaoo, ['hash' => $hash], ['pwdClear' => $pwd]); unset($hash, $piao, $pwd, $piaoo);
-	$all['exec_seconds_float'] = round($elapf, 2); unset($elapf);
+	$all['exeSFl'] = round($elapf, 2); unset($elapf);
 	echo(json_encode($all) . "\n");
-	unset($all, $argv);
+	unset($all);
 	return;
 }
 password_hash_test();
