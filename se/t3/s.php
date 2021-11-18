e6505137d9d2b5410ce942e56f0248c345d73ae8
kwynncom <buessresume@gmail.com>
2021-11-18 05:21:00 -0500

    more cleanup

--- se/t3/s.php
+++ se/t3/s.php
@@ -4,22 +4,13 @@ require_once('/opt/kwynn/kwutils.php');
 
 $f = '/tmp/st3';
 
+echo("v520\n");
 while (1) {
 	$h = fopen($f, 'r');
 	$t = trim(fgets($h));
 	fclose($h);
-	echo("\ncmd: $t\n");
-	if (!$t) {
-		continue;
-	}
-	echo('bex' . "\n");
+	if (!$t) continue;
 	$r = shell_exec($t);
-	echo('aex' . "\n");
-	$n = nanopk();
-	$of = '/tmp/' . sprintf('%020d', $n['tsc']) . '_' . sprintf('%03d', $n['pid']);
-	file_put_contents($of, $r);
-	echo('bwr' . "\n");
 	$oh = fopen('/tmp/o', 'w+');
 	fwrite($oh, $r, strlen($r));
-	echo('awr' . "\n");
 }
