44e353da3c9419484fd4e8f1e7e46ba8a2145ea0
kwynncom <buessresume@gmail.com>
2022-01-30 23:06:16 -0500

    Python circles

--- moon/moon.py
+++ moon/moon.py
@@ -1,13 +1,26 @@
 from skyfield.api import load
 from skyfield import almanac
+import json
 
 ts = load.timescale()
 eph = load('/opt/de421.bsp')
 
-t0 = ts.utc(2018, 9, 1)
-t1 = ts.utc(2018, 9, 10)
+t0 = ts.now() - 1.5
+t1 = t0 + 45
 t, y = almanac.find_discrete(t0, t1, almanac.moon_phases(eph))
 
 print(t.utc_iso())
-print(y)
-print([almanac.MOON_PHASES[yi] for yi in y])
+# print(y)
+# print([almanac.MOON_PHASES[yi] for yi in y])
+# print(t.utc_iso(), y, [almanac.MOON_PHASES[yi] for yi in y])
+# json.dumps(t.utc_iso(), y, [almanac.MOON_PHASES[yi] for yi in y])
+
+res = []
+# res.append(t.utc_iso().tolist())
+a1 = t.utc_iso()
+# l1 = a1.tolist()
+# res.append(y)
+# res.append([almanac.MOON_PHASES[yi] for yi in y])
+# json.dumps(res)
+# res20 = res.tolist()
+# json.dumps(a1)
