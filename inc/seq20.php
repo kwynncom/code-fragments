78cbda1bc5557703f318dd2b6b88ed96a1bc5046
kwynncom <buessresume@gmail.com>
2022-02-18 22:59:57 -0500

    sequence creation CAN FAIL

---
+++ inc/seq20.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('/opt/kwynn/kwutils.php');
+
+class test_seq extends dao_generic_3 implements fork_worker {
+	
+	const dbname = 'testseq';
+	
+	private function __construct(bool $init = false) {
+		parent::__construct(self::dbname);
+		$this->creTabs('seq');
+		if ($init) $this->scoll->drop();
+	}
+	
+	public static function shouldSplit (int $low, int $high, int $cpuCount) : bool { return true; }
+	public  function workitI (int $low, int $high) {
+
+		$cpun = multi_core_ranges::CPUCount();
+		
+		$snn = 1;
+		$wi = 0;
+		
+		for ($i = $low; $i <= $high; $i++) {
+			$sn = 'sn' . ($snn + random_int(0, $cpun));
+			$sr = $this->scoll->findOneAndUpdate(['_id' => $sn], [ '$inc' =>  ['seq' => 1 ]], 
+								['upsert' => true,  'returnDocument' => MongoDB\Operation\FindOneAndUpdate::RETURN_DOCUMENT_AFTER]);
+			$snn = $sr['seq'];
+		
+		}
+	}
+	
+	public static function workit  (int $low, int $high, int $workerN) { 
+		$o = new self();
+		$o->workitI($low, $high);
+	}
+
+	public static function kickoff() {
+		new self(true);
+		$n = random_int(1, M_MILLION);
+		echo("$n\n");
+		fork::dofork(true, 1, 6000, 'test_seq'); // , self::lfin, self::dbname, self::colla, $this->fts1);		
+	}
+
+	
+	
+}
+
+if (didCLICallMe(__FILE__)) test_seq::kickoff();
