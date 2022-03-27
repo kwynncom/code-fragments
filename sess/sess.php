a924941f03831e1ae92a40c99b861e008f2fd8ff
kwynncom <buessresume@gmail.com>
2022-03-26 20:37:17 -0400

    starting on session management, with location

---
+++ sess/sess.php
@@ -0,0 +1,5 @@
+<?php
+
+header('Content-Type: text/plain');
+
+print_r($_COOKIE);
---
+++ sess/sesssrv.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('/opt/kwynn/kwutils.php');
+
+header('Content-Type: text/plain');
+
+$a = kwjssrp();
+
+exit(0);
+
+
