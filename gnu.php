90aca452467edefc65e2de59a337a13f55e40584
kwynncom <buessresume@gmail.com>
2022-05-04 21:23:03 -0400

    bit more progress

--- gnu.php
+++ gnu.php
@@ -2,15 +2,34 @@
 
 require_once('/opt/kwynn/kwutils.php');
 
-$path = $argv[1];
-$gz = file_get_contents($path . 'gnucash.xml.gnucash'); unset($path);
-// $l = strlen($gz);
-$x  = gzdecode($gz); unset($gz);
-$o =  XMLReader::XML($x); unset($x);
-// $o->read();
-for($i=0; $i < 100000; $i++) {
-	echo($o->name . "\n");
-	$o->read();
-}
-exit(0);
+class GNUCashParse {
+	
+	public function __construct() {
+		$this->load();
+		$this->p10();
+	}
+	
+	private function load() {
+		
+		global $argv;
+		
+		$path = $argv[1];
+		$gz = file_get_contents($path . 'gnucash.xml.gnucash'); unset($path);
+		$x  = gzdecode($gz); unset($gz);
+		$o =  XMLReader::XML($x); unset($x);
+		$this->theo = $o;		
+	}
 
+	private function p10() {
+		
+		$o = $this->theo;
+
+		for($i=0; $i < 100000; $i++) {
+			echo($o->depth . ' ' . $o->name . ' ' . "\n");
+			$o->read();
+		}
+	}
+}
+
+
+new GNUCashParse();
