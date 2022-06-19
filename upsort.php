45b4d8db85dd9fd428e4a85cab774ac76fdee7c0
kwynncom <buessresume@gmail.com>
2022-06-19 02:20:54 -0400

    web up sort basics working

--- upsort.php
+++ upsort.php
@@ -17,20 +17,49 @@ foreach($a as $r) {
 	$a20[] = $r;
 } unset($a);
 
+$rootsz = strlen($root);
 $i = 0;
+
+$ht = '';
+$ht .= file_get_contents(__DIR__ . '/top.html');
+
 foreach($a20 as $r) {
+	
+	if (!trim($r)) continue;
+	
+	if (!$r) {
+		$l = strlen($r);
+		kwynn();
+	}
+	
 	$re = '/(\S+)\s+(\S+.*)/';
 	preg_match($re, $r, $ms);
+
+	if (!$ms) {
+		kwynn();
+	}
+
 	$hu = $ms[1];
 	$hu = str_replace('+', ' ', $hu);
 	$ts = strtotime($hu);
 	$rp = $ms[2];
+	$p = substr($rp, $rootsz);
 	unset($a20);
-	print_r(get_defined_vars());	
+	// print_r(get_defined_vars());	
 	
-	if (++$i > 0) break;
+	$ht .= '<tr>';
+	$ht .= '<td>';
+	$ht .= '<a href="' . $p . '">' . $p . '</a>';
+	$ht .= '</td>';
+	$ht .= '</tr>' . "\n";
+	
+	// if (++$i > 0) break;
 	continue;
 }
+
+$ht .= "</table>\n</body>\n</html>\n";
+echo($ht);
+
 exit(0);
 }
 
