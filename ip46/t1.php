c314582d291ae606e093a06d7a2d75396b12220f
kwynncom <buessresume@gmail.com>
2022-07-17 05:27:19 -0400

    comment on the comment

--- ip46/t1.php
+++ ip46/t1.php
@@ -1,6 +1,8 @@
 <?php
 
 // THE SOLUTION IS A SUBDOMAIN WITH ONLY a DNS AAAA entry!!!!!
+// Well, 2 subdomains, one with just an A and the other with just a AAAA
+
 
 	if (0) $KW_G_SPATH = '/server.php';
 	else $KW_G_SPATH = '/t/22/07/ip46/server.php';
