f40e7e4be0781c10bc573b271cdd0057e2ebfda8
kwynncom <buessresume@gmail.com>
2022-07-28 01:08:27 -0400

    working on web update sort

--- upsort.php
+++ upsort.php
@@ -1,67 +1,83 @@
-<?php
+<?php // 2022/07/28 01:07
 
-if (time() > strtotime('2022-06-19 03:59')) die('expired');
+// Hopefully a less in objectification.
 
-function t1() {
+if (time() > strtotime('2022-07-28 03:59')) die('expired');
 
-$root = $_SERVER['DOCUMENT_ROOT'];
+require_once('/opt/kwynn/kwutils.php');
 
-$c = 'find ' . $root . '/ ' . ' -type f -printf "%T+\t%p\n" | sort -r ';
-$res = shell_exec($c);
-$a = explode("\n", $res); unset($res);
+class siteUpdateTracker {
 
-
-$a20 = [];
-foreach($a as $r) {
-	if (preg_match('/\/\.git\//', $r)) continue;
-	$a20[] = $r;
-} unset($a);
-
-$rootsz = strlen($root);
-$i = 0;
-
-$ht = '';
-$ht .= file_get_contents(__DIR__ . '/top.html');
-
-foreach($a20 as $r) {
+public function __construct() {
 	
-	if (!trim($r)) continue;
-	
-	if (!$r) {
-		$l = strlen($r);
-		kwynn();
-	}
-	
-	$re = '/(\S+)\s+(\S+.*)/';
-	preg_match($re, $r, $ms);
-
-	if (!$ms) {
-		kwynn();
-	}
-
-	$hu = $ms[1];
-	$hu = str_replace('+', ' ', $hu);
-	$ts = strtotime($hu);
-	$rp = $ms[2];
-	$p = substr($rp, $rootsz);
-	unset($a20);
-	// print_r(get_defined_vars());	
-	
-	$ht .= '<tr>';
-	$ht .= '<td>';
-	$ht .= '<a href="' . $p . '">' . $p . '</a>';
-	$ht .= '</td>';
-	$ht .= '</tr>' . "\n";
-	
-	// if (++$i > 0) break;
-	continue;
+	$this->set10();
+	$this->t1();
 }
 
-$ht .= "</table>\n</body>\n</html>\n";
-echo($ht);
-
-exit(0);
+private function set10() {
+	$this->droot = $root = $_SERVER['DOCUMENT_ROOT'];
+	$c = 'find ' . $root . '/ ' . ' -type f -printf "%T+\t%p\n" | sort -r ';
+	$res = shell_exec($c); unset($c);
+	$this->thea = explode("\n", $res); unset($res);
 }
 
-t1();
+private function t1() {
+
+	$a = $this->thea;
+
+	$a20 = [];
+	foreach($a as $r) {
+		if (preg_match('/\/\.git\//', $r)) continue;
+		$a20[] = $r;
+	} unset($a);
+
+	$rootsz = strlen($this->droot);
+	$i = 0;
+
+	$ht = '';
+	$ht .= file_get_contents(__DIR__ . '/top.html');
+
+	foreach($a20 as $r) {
+
+		if (!trim($r)) continue;
+
+		if (!$r) {
+			$l = strlen($r);
+			kwynn();
+		}
+
+		$re = '/(\S+)\s+(\S+.*)/';
+		preg_match($re, $r, $ms);
+
+		if (!$ms) {
+			kwynn();
+		}
+
+		$hu = $ms[1];
+		$hu = str_replace('+', ' ', $hu);
+		$ts = strtotime($hu);
+		$rp = $ms[2];
+		$p = substr($rp, $rootsz);
+		unset($a20);
+		// print_r(get_defined_vars());	
+
+		$ht .= '<tr>';
+		$ht .= '<td>';
+		$ht .= '<a href="' . $p . '">' . $p . '</a>';
+		$ht .= '</td>';
+		$ht .= '</tr>' . "\n";
+
+		// if (++$i > 0) break;
+		continue;
+	}
+
+	$ht .= "</table>\n</body>\n</html>\n";
+	echo($ht);
+
+	exit(0);
+	} // func
+} // class
+
+new siteUpdateTracker();
+
 
