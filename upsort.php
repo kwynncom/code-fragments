a28a5b9b3903aa10977b513df5a6045ae0083ecc
kwynncom <buessresume@gmail.com>
2022-07-28 01:15:36 -0400

    separated HTML

--- upsort.php
+++ upsort.php
@@ -1,6 +1,6 @@
-<?php // 2022/07/28 01:07
+<?php // 2022/07/28 01:15
 
-// Hopefully a less in objectification.
+// Hopefully a lesson in objectification--going from a function to a class / object.
 
 if (time() > strtotime('2022-07-28 03:59')) die('expired');
 
@@ -11,19 +11,36 @@ class siteUpdateTracker {
 public function __construct() {
 	
 	$this->set10();
-	$this->t1();
+	$this->p20();
+	$this->doHT();
 }
 
 private function set10() {
 	$this->droot = $root = $_SERVER['DOCUMENT_ROOT'];
 	$c = 'find ' . $root . '/ ' . ' -type f -printf "%T+\t%p\n" | sort -r ';
 	$res = shell_exec($c); unset($c);
-	$this->thea = explode("\n", $res); unset($res);
+	$this->therawa = explode("\n", $res); unset($res);
 }
 
-private function t1() {
+private function doHT() {
+	$ht = '';
+	$ht .= file_get_contents(__DIR__ . '/top.html');
+	foreach($this->theps as $p) {
+		$ht .= '<tr>';
+		$ht .= '<td>';
+		$ht .= '<a href="' . $p . '">' . $p . '</a>';
+		$ht .= '</td>';
+		$ht .= '</tr>' . "\n";		
+	}
+	
+	$ht .= "</table>\n</body>\n</html>\n";
+	echo($ht);
+	
+}
 
-	$a = $this->thea;
+private function p20() {
+
+	$a = $this->therawa;
 
 	$a20 = [];
 	foreach($a as $r) {
@@ -31,51 +48,26 @@ private function t1() {
 		$a20[] = $r;
 	} unset($a);
 
+	$this->awog = $a20;
+	
 	$rootsz = strlen($this->droot);
 	$i = 0;
-
-	$ht = '';
-	$ht .= file_get_contents(__DIR__ . '/top.html');
 
 	foreach($a20 as $r) {
 
 		if (!trim($r)) continue;
 
-		if (!$r) {
-			$l = strlen($r);
-			kwynn();
-		}
-
 		$re = '/(\S+)\s+(\S+.*)/';
 		preg_match($re, $r, $ms);
-
-		if (!$ms) {
-			kwynn();
-		}
-
 		$hu = $ms[1];
 		$hu = str_replace('+', ' ', $hu);
 		$ts = strtotime($hu);
 		$rp = $ms[2];
 		$p = substr($rp, $rootsz);
-		unset($a20);
-		// print_r(get_defined_vars());	
-
-		$ht .= '<tr>';
-		$ht .= '<td>';
-		$ht .= '<a href="' . $p . '">' . $p . '</a>';
-		$ht .= '</td>';
-		$ht .= '</tr>' . "\n";
-
-		// if (++$i > 0) break;
-		continue;
+		
+		$this->theps[] = $p;
 	}
-
-	$ht .= "</table>\n</body>\n</html>\n";
-	echo($ht);
-
-	exit(0);
-	} // func
+} // func
 } // class
 
 new siteUpdateTracker();
