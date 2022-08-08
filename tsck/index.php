d88523069d1bb427f462c22cbfb00c77fc6dbf94
kwynncom <buessresume@gmail.com>
2022-08-08 02:25:45 -0400

    live

--- tsck/index.php
+++ tsck/index.php
@@ -9,17 +9,23 @@
 <style>
     body { font-family: sans-serif; }
 	pre { 
-		font-size: 3vw;
+		font-size: 4vw;
 		margin-top: 0;
 		margin-bottom: 0;
+		display: inline-block;
 		
 	}
 </style>
 </head>
 <body>
+	<div style='position: relative; '>
 	<pre><!--
 		--><?php require_once('tsck.php');	?><!--
 	--></pre>
+	<div style='display: inline-block; position: absolute; top: 1ex; '>
+		<button onclick='history.go(0);'><span style='font-size: 3vw;'>&#10227;</span></button>
+	</div>
+	</div>
 	<div>
 		<p>testing <a href='https://github.com/kwynncom/simple-time-server'>
 				my simple timeserver</a></p>
@@ -27,7 +33,11 @@
 		<p><a href='https://github.com/kwynncom/code-fragments/tree/a87f9fead64cfd77d3eebc9f6b068389508581df/tsck'>a specific version of the code</a>
 			
 		</p>
+		<p><a href='/t/20/08/dates/'>PHP date format codes</a></p>
 		<p><a href='/'>home</a></p>
+		<p>02:25 - version stamp
+			
+		</p>
 	</div>
 </body>
 </html>
