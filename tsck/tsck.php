63dfaaf679a31797bae7c454539a350c355d7b31
kwynncom <buessresume@gmail.com>
2022-09-04 01:03:27 -0400

    reversing debugging

--- tsck/tsck.php
+++ tsck/tsck.php
@@ -26,35 +26,26 @@ class stsck {
 			$c = self::cmdprefix . $n . '' . $dp . '';
 			// echo($n);
 			$rra[] = $rr = shell_exec($c);
-			echo($c . ' ' . $rr . "\n");
+			echo($rr);
 			$this->cmdso[] = $c;
 		} // loop
 		$this->orra = $rra;
 	} // func
 	
-	public function getCmdsS() { 
-		$s = '';
-		foreach($this->cmdso as $i => $v) {
-			$s .= ($i + 0) . ' ' . $v . "\n";
-		}
-		
-		return $s;
-		
-	}
+	public function getCmdsS() { return implode("\n", $this->cmdso);}
 	
 	private function do20() {
 		$fra = [];
 		$rs = [];
 		foreach($this->orra as $r) {
-			if (!isset($r) || !$r) continue;
 			$ns = intval(trim($r));
 			$U  = intval(floor($ns / M_BILLION));
 			$rs[] = date('D n/j H:i:s', $U);
 			$fra[] = ($ns - $U * M_BILLION) / M_BILLION;
 		} unset($a, $r, $U); // loop
 		
-		for($i=0; $i < 4; $i++) if (isset($rs [$i])) echo($i . ': ' . $rs [$i] . "\n");
-		for($i=0; $i < 4; $i++) if (isset($fra[$i])) printf("%d %0.9f\n", $i, $fra[$i]);
+		for($i=0; $i < 4; $i++) echo($rs [$i] . "\n");
+		for($i=0; $i < 4; $i++) printf("%0.9f\n", $fra[$i]);
 		echo(date('T P') . "\n");
 		echo(date('e') . "\n");
 	} // func
