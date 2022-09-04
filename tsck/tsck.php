b4cfc05e92a63e3ba26820058fad49ac333eae37
kwynncom <buessresume@gmail.com>
2022-09-04 00:57:08 -0400

    debugging version

--- tsck/tsck.php
+++ tsck/tsck.php
@@ -26,26 +26,35 @@ class stsck {
 			$c = self::cmdprefix . $n . '' . $dp . '';
 			// echo($n);
 			$rra[] = $rr = shell_exec($c);
-			echo($rr);
+			echo($c . ' ' . $rr . "\n");
 			$this->cmdso[] = $c;
 		} // loop
 		$this->orra = $rra;
 	} // func
 	
-	public function getCmdsS() { return implode("\n", $this->cmdso);}
+	public function getCmdsS() { 
+		$s = '';
+		foreach($this->cmdso as $i => $v) {
+			$s .= ($i + 0) . ' ' . $v . "\n";
+		}
+		
+		return $s;
+		
+	}
 	
 	private function do20() {
 		$fra = [];
 		$rs = [];
 		foreach($this->orra as $r) {
+			if (!isset($r) || !$r) continue;
 			$ns = intval(trim($r));
 			$U  = intval(floor($ns / M_BILLION));
 			$rs[] = date('D n/j H:i:s', $U);
 			$fra[] = ($ns - $U * M_BILLION) / M_BILLION;
 		} unset($a, $r, $U); // loop
 		
-		for($i=0; $i < 4; $i++) echo($rs [$i] . "\n");
-		for($i=0; $i < 4; $i++) printf("%0.9f\n", $fra[$i]);
+		for($i=0; $i < 4; $i++) if (isset($rs [$i])) echo($i . ': ' . $rs [$i] . "\n");
+		for($i=0; $i < 4; $i++) if (isset($fra[$i])) printf("%d %0.9f\n", $i, $fra[$i]);
 		echo(date('T P') . "\n");
 		echo(date('e') . "\n");
 	} // func
