ddbc1ae112d64c4e59d2cad36416c7fb04d947ce
kwynncom <buessresume@gmail.com>
2022-09-05 05:36:06 -0400

    got it! try 4 or 5

--- nodesock/t1.js
+++ nodesock/t1.js
@@ -1,12 +1,10 @@
 var net = require('net'); // TCP
 var udp = require('dgram');
-const { toUSVString } = require('util');
 
-class check8123 { // executed below
+class sock { // executed below
 
     config() {
         this.ds = {'4' : 'ipv4.kwynn.com', '6' : 'ipv6.kwynn.com'};
-        this.ds = {  '6' : 'ipv6.kwynn.com'};
         this.port = 8123;
         this.minns = 1662349142572807324; // before 2022/09/05 00:21 EDT / New York - this is in the past
         this.stopat = 4;
@@ -18,61 +16,28 @@ class check8123 { // executed below
 
     constructor() {
         this.config();
-<<<<<<< HEAD
-        this.thePromises = [];
-        this.prarm = [];
-        this.oro = [];
-        // this.setUDP();
-        this.setTCP();
-        this.prarm.forEach((f) => { 
-            f(); 
-        });
-=======
         this.setPr();
         this.dotcp();
         this.doudp();
->>>>>>> tmp
     }
 
-    setUDP() {
+    doudp() {
 
         for (const [ipv, dom] of Object.entries(this.ds)) {
             var server = udp.createSocket('udp' + ipv);
-            const f  = (msg) => { return this.procRes(msg, dom, 'udp', ipv);   };
-            let prr;
-            const pr = new Promise((resolve) => { prr = resolve }).then((pr) => { 
-                pr = f(pr); 
-                return pr;
-            });
-            this.thePromises.push(pr);
-
-            this.prarm.push(() => {
-                    server.on('message', prr);
-                    server.send('a', this.port, dom);
-                }
-            );
+            server.on('message', (msg) => { this.procRes(msg, dom, 'udp', ipv);   });
+            server.send('a', this.port, dom);
         }
     }
 
-    setTCP() {
-        for (const [ipv, dom] of Object.entries(this.ds)) this.set1tcp(dom, ipv);
+    dotcp() {
+        for (const [ipv, dom] of Object.entries(this.ds)) this.do1tcp(dom, ipv);
     }
 
-    set1tcp(domain, ipv) {
-        
+    do1tcp(domain, ipv) {
         const client = net.connect({port: this.port, host: domain}, () => {
-            const f =  (netr) => { return this.procRes(res, domain, 'tcp', ipv); };
-            let prr;
-            const pr = new Promise((resolve) => { prr = resolve }).then((res) => { 
-                res = f(res); 
-                return res;
-            });
-            this.thePromises.push(pr);
-            this.prarm.push(() => {
-                    client.on('data', prr);
-                    client.write('a');
-                }
-            );
+            client.on('data', (res) => { this.procRes(res, domain, 'tcp', ipv); });
+            client.write('a');
         });
     }
 
@@ -85,28 +50,6 @@ class check8123 { // executed below
         if (theint < this.minns) return;
 
         const ro = { 'ns' : s2, 'dom' : domain, 'ty' : ptype, 'ipv' : ipv};
-<<<<<<< HEAD
-        this.oro.push(ro);
-        return ro;
-    }
-
-    getResI() {
-        let dat = Promise.all(this.thePromises).then((good) => 
-        {
-            const good1 = 1;
-        }, (bad) => {
-            const bad1 = 1;
-        });
-        return dat;
-    }
-
-    getRes() {
-        const o = this.getResI();
-        const json = JSON.stringify(this.oro);
-        return json;
-    }
-
-=======
 
         this.theres.push(ro);
         if (++this.reci >= this.stopat) {
@@ -146,7 +89,6 @@ class check8123 { // executed below
         };
         return response;
     }
->>>>>>> tmp
 }
 
 const o = new sock();
@@ -156,25 +98,3 @@ exports.handler = o.lam;
 if (!process.env.AWS_LAMBDA_FUNCTION_NAME) o.lam();
 
 // GOT IT, with async! 5:30am 9/5/2022
-
-<<<<<<< HEAD
-const o = new check8123();
-
-const finaljs = o.getResI().then((x) => {
-    const good503 = 1;
-});
-
-exports.handler = async (event) => { // AWS Lambda return
-    const response = {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: finaljs,
-    };
-    return response;
-};
-
-if (!process.env.AWS_LAMBDA_FUNCTION_NAME) console.log(finaljs);
-=======
->>>>>>> tmp
