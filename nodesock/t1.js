22c5c662ca76333cca0c2d2691520c02996a32c2
kwynncom <buessresume@gmail.com>
2022-09-04 23:45:41 -0400

    weird rounded integer version

--- nodesock/t1.js
+++ nodesock/t1.js
@@ -9,12 +9,15 @@ class sock {
 
         const ds = ['ipv4.kwynn.com', 'ipv6.kwynn.com'];
 
-        setTimeout(() => { process.exit(); }, 1000);
+        let reci = 0;
 
         for (let i=0; i < ds.length; i++) {
             const client = net.connect({port: 8123, host: ds[i]}, () => {
                 client.on('data', (dat) => { 
-                    console.log(dat.toString().trim());
+                    const theint = parseInt(dat.toString().trim());
+                    if (theint < 1662349142572807324) return;
+                    console.log(theint);
+                    if (++reci === ds.length) process.exit();
                 });
                 client.write('a');
             });
