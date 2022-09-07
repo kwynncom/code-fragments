33c126a083a065b77271a9e057af09151d3cd142
kwynncom <buessresume@gmail.com>
2022-09-07 04:57:05 -0400

    one more try - Lambda does not seem to support outgoing IPv6

---
+++ nodesock/mini.js
@@ -0,0 +1,35 @@
+const net  = require('net');
+
+const myf = async (event) => {
+    
+    const ipv4 = '34.193.238.16';
+    const ipv6 = '2600:1f18:23ab:9500:7a93:a206:f823:20c3';
+    const dom = 'kwynn.com';
+    
+    let onfin;
+    const thepr = new Promise((resolve) => { onfin = resolve; });
+    
+    let cs = 'nothing';
+    
+    const client = net.connect({port: 8123, host: dom}, () => {
+        cs = 'connected';
+        onfin(cs);
+    });
+    
+    await thepr;
+    
+    if (!process.env.AWS_LAMBDA_FUNCTION) console.log(cs);
+
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(cs),
+    };
+    return response;
+};
+
+if (!process.env.AWS_LAMBDA_FUNCTION_NAME) {
+    myf();
+}
+
+exports.handler = myf;
