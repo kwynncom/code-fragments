e7e00380383d20a549cd843744a7c5d7b20472e4
kwynncom <buessresume@gmail.com>
2022-09-07 02:24:57 -0400

    abstracting

--- nodesock/base.js
+++ nodesock/base.js
@@ -2,6 +2,7 @@ const kwuf = require('./utils.js');
 const kwas = kwuf.kwas;
 const time = kwuf.time;
 const islam = kwuf.islam;
+const cl   = kwuf.cl;
 
 class ck8123Base {
     config() {
@@ -15,6 +16,40 @@ class ck8123Base {
         this.theres = [];
         if (!islam()) setTimeout(() => { process.exit()}, 3000 );
     }    
+
+    procRes(resraw, domain, ptype, ipv) { // see note at bottom about this processing
+        const re = new RegExp(/\d+/);
+        const s1 = resraw.toString();
+        const a2 = s1.match(re);
+        const s2 = a2[0];
+        const theint = parseInt(s2);
+        if (theint < this.minns) return;
+
+        const ro = { 'ns' : s2, 'dom' : domain, 'ty' : ptype, 'ipv' : ipv};
+
+        if (!islam()) cl(ro);
+
+        this.theres.push(ro);
+        if (++this.reci >= this.stopat) {
+            this.onfin();
+            return;
+        }
+    }
+
+    getResI() { return this.theres; }
+
+    setPr() {
+        this.thepr = new Promise((resolve) => { 
+            this.onfin = resolve;
+        }).then(() => {
+                return this.theres;
+            }
+        );
+    }
+
+    async getRes() { return await this.thepr; }
+
+
 }
 
 module.exports.ck8123Base = ck8123Base;
--- nodesock/index.js
+++ nodesock/index.js
@@ -14,81 +14,21 @@ class sock extends ck8123Base {
         this.config();
         this.setPr();
         this.dotcp();
-        this.doudp();
     }
 
-    doudp() {
-
-        if (typeof udp === 'undefined') return;
-
+    dotcp() { 
         for (const [ipv, dom] of Object.entries(this.ds)) {
-            var server = udp.createSocket('udp' + ipv);
-            server.on('message', (msg) => { this.procRes(msg, dom, 'udp', ipv);   });
-            server.send('a', this.port, dom);
+            const cb = (res) => { this.procRes(res, dom, 'tcp', ipv); };
+            this.do1tcp(dom, this.port, ipv, cb);  
             this.stopat++;
         }
     }
 
-    dotcp() {
-        for (const [ipv, dom] of Object.entries(this.ds)) this.do1tcp(dom, ipv);
-    }
-
-    do1tcp(domain, ipv) {
-        const client = net.connect({port: this.port, host: domain}, () => {
-            client.on('data', (res) => { this.procRes(res, domain, 'tcp', ipv); });
+    do1tcp(domain, port, ipv, cb) {
+        const client = net.connect({port: port, host: domain}, () => {
+            client.on('data', cb);
             client.write('a');
-            this.stopat++;
         });
-    }
-
-    procRes(resraw, domain, ptype, ipv) { // see note at bottom about this processing
-        const re = new RegExp(/\d+/);
-        const s1 = resraw.toString();
-        const a2 = s1.match(re);
-        const s2 = a2[0];
-        const theint = parseInt(s2);
-        if (theint < this.minns) return;
-
-        const ro = { 'ns' : s2, 'dom' : domain, 'ty' : ptype, 'ipv' : ipv};
-
-        if (!islam()) cl(ro);
-
-        this.theres.push(ro);
-        if (++this.reci >= this.stopat) {
-            this.onfin();
-            return;
-        }
-    }
-
-    getResI() { return this.theres; }
-
-    setPr() {
-        this.thepr = new Promise((resolve) => { 
-            this.onfin = resolve;
-        }).then(() => {
-                return this.theres;
-            }
-        );
-    }
-
-    async getRes() { return await this.thepr; }
-
-    async lam() {
-
-        const dat = await this.getRes();
-
-        if (!islam()) {
-            console.log(dat);
-        }
-
-        const response = {
-            statusCode: 200,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: dat,
-        };
-        return response;
     }
 }
 
