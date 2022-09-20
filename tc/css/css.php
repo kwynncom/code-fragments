144416fbdbf30fa57a6b8474487a67c4728e51fa
kwynncom <buessresume@gmail.com>
2022-09-20 06:32:11 -0400

    new concept starting to work

--- tc/css/css.php
+++ tc/css/css.php
@@ -3,20 +3,30 @@
 require_once('/opt/kwynn/kwutils.php');
 
 function docss() {
-    $fs = glob(__DIR__ . '/*.css');
-    foreach($fs as $f) ftou($f);
+
+    $r = $_SERVER['DOCUMENT_ROOT'];
+    $s =  dirname($_SERVER['REQUEST_URI']);
+    $d = $r . '' . $s;
+    
+    $fs = rsearch($d, '/^.*\.css$/');
+    foreach($fs as $f) {
+        $d = str_replace($r, '', $f);
+        $t = "<link rel='stylesheet' href='$d' />\n";
+        echo($t);
+    }
 }
 
 docss();
 
-function ftou($f) {
-    $r = $_SERVER['DOCUMENT_ROOT'];
-    if ($l = readlink($r)) $r = $l;
-    $p10 = str_replace($r, '', $f);
-    $up = $_SERVER['REQUEST_URI'];
-    $s =  $_SERVER['PHP_SELF'];
-    $d = dirname($s);
-    $fd = str_replace($r, '', $f);
-    
-    return;
+function rsearch($dir, $re) {
+    $return = [];
+    $iti = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+    while($iti->valid()){
+        $p = $iti->key();
+        if (preg_match($re, $p, $ms)) {
+            $return[] = $ms[0];
+        }
+        $iti->next();
+    }
+    return $return;
 }
