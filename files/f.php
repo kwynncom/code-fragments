f1c7a43b60de539127c32263aa2b7e81df927aff
kwynncom <buessresume@gmail.com>
2022-10-12 22:02:20 -0400

    much, much tighter

--- files/f.php
+++ files/f.php
@@ -3,20 +3,14 @@
 require_once('/opt/kwynn/kwutils.php');
 
 class filePtrTracker extends dao_generic_3 {
-	
-	const maxLnn = 200;
 	const dbname = 'files';
+	const maxLnn = 200;
+	const defaultNLines = 40;
 	public readonly string $name;
 	private readonly mixed $ohan;
-	private readonly int $endInit;
 	private readonly bool $collExists;
-	
-	const defaultNLines = 40;
-	
-	public function __destruct() {
-		fclose($this->ohan);
-	}
-	
+	private readonly array $oq;
+
 	public function __construct(string $name) {
 		$this->name = $name;
 		$this->dbmg();
@@ -24,6 +18,8 @@ class filePtrTracker extends dao_generic_3 {
 		$this->getEndInit();
 	}
 
+	public function __destruct() { fclose($this->ohan);	}
+	
 	private function dbmg() {
 		parent::__construct(self::dbname);
 		$this->creTabs('files');
@@ -32,51 +28,32 @@ class filePtrTracker extends dao_generic_3 {
 	
 	private function getEndInit() {
 		$res = $this->fcoll->findOne($this->oq);
-		if (!$res) $this->tailI(self::defaultNLines);
-		else $this->initEnd = $res['end'];
-		fseek($this->ohan, $this->initEnd);
-		
+		if (!$res) $this->setInitViaTail(self::defaultNLines);
+		else fseek($this->ohan, $res['end']);
 	}
 	
-	private function setEndF(int $endin = null) {
-		
-		$q = $this->oq;
-
-		$ce = isset($this->collExists);
-		if (!$ce && $this->fcoll->count($q) === 0) {
+	private function setInitViaTail() {
+		$h = $this->ohan;
+		fseek($h, -1 * self::defaultNLines * self::maxLnn, SEEK_END);
+		$fn = ftell($h);
+		if ($fn < 0) { fseek($h, 0, SEEK_SET); return 0; }
+		kwas(fgets($h), 'throwaway line nonexistent'); // throw away because we may be in middle
+	}
+	
+	private function setEndF() {
+	
+		if (!isset($this->collExists) && $this->fcoll->count($this->oq) === 0) {
 			$dat['_id'] = $this->name;
 			$this->fcoll->insertOne($dat);
-			$end = $this->initEnd;
 		}  
-		
-		if ($endin) { 
-			$end = $endin;
-			if (!$ce) $this->initEnd = $end;
-		}
-		if (!$ce) $this->collExists = true;
-		
-		kwas(isset($end), 'setEndF end should be defined here');
-		$this->fcoll->upsert($q, ['end' => $end]);
-		if (!isset($this->ofptr)) $this->ofptr = $end;
+		$this->collExists = true;
+		$this->fcoll->upsert($this->oq, ['end' => ftell($this->ohan)]);
 	}
 	
 	public function fgets() {
 		$l = fgets($this->ohan);
-		$this->setEndF(ftell($this->ohan));
+		$this->setEndF();
 		return $l;
-	}
-	
-	private function tailI() {
-		$h = $this->ohan;
-		fseek($h, 0, SEEK_END);
-		$end = ftell($h);
-		if ($end === 0) { $this->initEnd = 0; return; }
-		
-		fseek($h, -1 * self::defaultNLines * self::maxLnn, SEEK_END);
-		$fn = ftell($h);
-		if ($fn < 0) { fseek($h, 0, SEEK_SET); $this->initEnd = 0; return; }
-		kwas(fgets($h), 'throwaway line nonexistent'); // throw away because we may be in middle
-		$this->initEnd = ftell($h);
 	}
 }
 
@@ -86,8 +63,5 @@ function testFPT() {
 }
 
 if (didCLICallMe(__FILE__)) testFPT();
-
-
-
 
 exit(0);
