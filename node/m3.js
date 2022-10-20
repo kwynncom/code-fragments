a319c668e0b70a726203e16e1e4a2921b550005f
kwynncom <buessresume@gmail.com>
2022-10-20 01:35:04 -0400

    lots of cleanup

--- node/m3.js
+++ node/m3.js
@@ -9,55 +9,24 @@ const sntpWorst = require('./myqueries/sntp_worst.js');
 
 class myMongoDBServer {
   constructor() {
-    this.setMongoDBBase();
-    this.htserver();
-
-  }
-
-
-
-  setMongoDBQs() {
-    this.sntpWoO = new sntpWorst(this.client);
-
-}   
-
-  setMongoDBBase() {
     const self = this;
-    MongoClient.connect(mongoConnURL, function(err, client) { 
-      self.client = client;
-      self.setMongoDBQs();    
-    }); 
-    
-    return;
+    MongoClient.connect(mongoConnURL, function(err, client) { self.sntpWoO = new sntpWorst(client); }); 
+    this.initHTServer();
   }
 
-  async doq(req) {
-
-    const a = await this.sntpWoO.get();
-
-    const  res = JSON.stringify(a);
-    return res;
-  }
+ async doq(req) {   return JSON.stringify(await this.sntpWoO.get());  }
 
  async doHTr(req, res) {
-
     const json = await this.doq(req);
-
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(json);
   }
 
-  async htserver() {
+  initHTServer() {
     const self = this;
-    const server = http.createServer((req, res) => {
-        return self.doHTr(req, res);
-    });
-
-    server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-    });
-
+    const server = http.createServer((req, res) => { self.doHTr(req, res);   });
+    server.listen(port, hostname, () => { console.log(`Server running at http://${hostname}:${port}/`);  });
   } // func
 } // class
 
--- node/myqueries/sntp_worst.js
+++ node/myqueries/sntp_worst.js
@@ -1,26 +1,13 @@
-module.exports = class sntp_worst {
-    constructor(cli) {
-        this.coll = cli.db('sntp4').collection('calls');
-    }
-
+module.exports = class sntpWorst {
+    constructor(cli) { this.coll = cli.db('sntp4').collection('calls');   }
     async get() {
-        const cur = this.coll.aggregate(
+        return await this.coll.aggregate(
             [
                 {$match : {'U' : {'$gte' : new Date().getTime() / 1000 - 86400}, offset : {$exists : true}}},
                 {$project : {absoff : {$abs : '$offset'}}},
                 {$sort : {absoff : -1}},
                 {$limit : 20}
             ]
-        );  
-        
-        const a = [];
-
-        for await (const doc of cur) {
-            a.push(doc);
-        }
-
-       return a;
+        ).toArray();  
     }
-
-
 }
