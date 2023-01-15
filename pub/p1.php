a7b729f6e2499ed0b8543a28ffd359bb5c4ae7fb
kwynncom <buessresume@gmail.com>
2023-01-15 02:59:00 -0500

    experiment is a success

--- pub/p1.php
+++ pub/p1.php
@@ -1,9 +1,20 @@
 <?php
 
+$prf = 'PRIVATE.txt';
+$puf = 'pub.pem';
+$bits = 2048;
 
-$private = openssl_pkey_new(['private_key_bits' => 2048]);
-$pubpem = openssl_pkey_get_details($private)['key'];
-file_put_contents('pub.pem', $pubpem);
-$privo;
-openssl_pkey_export($private, $privo);
-file_put_contents('PRIVATE.txt', $privo);
+if (1) {
+	$private = openssl_pkey_new(['private_key_bits' => $bits]); unset($bits);
+	$pubpem = openssl_pkey_get_details($private)['key'];
+	file_put_contents($puf, $pubpem); unset($pubpem);
+	openssl_pkey_export_to_file($private, $prf); unset($private);
+}
+
+$pro = openssl_pkey_get_private(file_get_contents($prf));  unset($prf);
+$puo = openssl_pkey_get_public (file_get_contents($puf)); unset($puf);
+
+$secret = 'I am the secret 4.';
+openssl_public_encrypt ($secret, $cipherText, $puo); unset($secret, $puo);
+openssl_private_decrypt($cipherText, $plainText, $pro); unset($pro, $ciperText);
+echo($plainText);
