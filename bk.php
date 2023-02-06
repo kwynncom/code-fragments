ce4f8745a728cbda2a36f7f391464b82872096ca
kwynncom <buessresume@gmail.com>
2023-02-05 21:32:19 -0500

    starting to filter backup

--- bk.php
+++ bk.php
@@ -1,13 +1,52 @@
 <?php
 
-// echo(shell_exec('find ~ | head')); // takes forever
-// echo(shell_exec('find /var/kwynn')); // testing
+require_once('/opt/kwynn/kwutils.php');
 
-$b = trim(shell_exec('echo ~')) . '/';
-echo($b . "\n");
+class kwBackupSome {
+	
+	public function __construct() {
+		$this->base10();
+		$this->do10();
+		pclose($this->oh);
+	}
+	
+	private function base10() {
+		$this->obase = trim(shell_exec('echo ~')) . '/';
+		echo($this->obase . "\n");
+		$this->oh = popen('find ~ -maxdepth 1 ', 'r');
+	}
 
+	private function dobranch($pin) {
+		static $ps = [];
+		static $ls = [];
+		static $b = ['.bash_history', '.aws', '.ssh', '.bashrc' ];
+		
+		if (!$ps) foreach($b as $p) {
+			$s = $this->obase . $p . '';
+			$ps[] = $s;
+			$ls[] = strlen($s);
+		}
+		
+		kwynn();
+		
+		foreach($ps as $i => $p) {
+			$t10 = substr($pin, 0, $ls[$i]);
+			if ($t10 === $ps[$i]) return true;
+		}
+		return false;
+		
+		
+	}
+	
+	private function do10() {
 
+		while($s = fgets($this->oh)) {
+			if ($this->dobranch($s)) echo($s);
+			// echo($s);
+		}
 
-$h = popen('find ~ -maxdepth 1 ', 'r');
-while($s = fgets($h)) echo($s);
-pclose($h);
+	}
+
+}
+
+new kwBackupSome(); // .bash_history is special
