d60b174eea8128ac833d0a5200e335fd069cf113
kwynncom <buessresume@gmail.com>
2023-04-21 23:37:20 -0400

    perhaps not elegant but names are working and partially abstracted

--- IQ/p10.php
+++ IQ/p10.php
@@ -1,73 +1,57 @@
 <?php // https://www.thomas.co/sites/default/files/thomas-files/2022-09/Aptitude%20Example%20Booklet_2021%20V1.pdf
 
+require_once('/opt/kwynn/kwutils.php');
+
 class iq1 {
 	
-	const nms  = [
+	const names  = [
 			['Rachel', 'Wendy'],
 			['Fred', 'John', 'Pete', 'Tom', 'Bob', 'Paul']
 		];
 	                          /* not as */
-	const trs  = [	[['lighter','heavy'   ], ['heavier' , 'light']],
+	const traits = [
+					[['lighter','heavy'   ], ['heavier' , 'light']],
 					[['duller', 'bright'  ], ['brighter', 'dull']],
 					[['sadder', 'happy'   ], ['happier' , 'sad']], 
 					[['weaker', 'strong'  ], ['stronger', 'weak']],
 					[['sadder', 'happy'   ], ['happier' , 'sad']]
 		];
 	
-	/* private function pickAdj() {
-		$li = 0;
-		$la = self::trs;
-		$t = '';
-		$ca = [];
+	private function pickLevs(array $ain) {
+		$ra = [];
+		$cki = 0;
+		$ta = $ain;
+		do { 
+			$ra[] = $ti = random_int(0, count($ta) - 1);
+			$ta = kwifs($ta, $ti);
+			if (!is_array($ta)) break;
+		} while($cki++ < 7);
 		
-		for($i=0; $i < count($la); $i++) {
-			$ti = random_int(0, count($la) - 1);
-			$ia[] = $ti;
-			$la = $la[$ti];
-		}
-		
-		return;
-	} */
+		return $ra;
+	}
 	
 	public function __construct() {
 		$this->do10();
+	//	$this->do20();
+	}
+	
+	private function do20() {
+		$aa0 = $this->pickLevs(self::traits);
 	}
 	
 	private function do10() {
 		
-		$ap[] = $p1a = $this->pickLevs(self::nms);
-		$ap[] =		   $this->pickLevs(self::nms, $p1a); unset($p1a);
-		
-		$aa[] = $a1a = $this->pickLevs(self::trs);
-		$aa[] =		   $this->pickLevs(self::trs, $a1a); unset($a1a);
-		
-		$p1 = self::nms[$ap[0][0]][$ap[0][1]];
-		$p2 = self::nms[$ap[1][0]][$ap[1][1]];
-		
-		echo($p1 . ' ' . $p2);
-	}
-	
-	private function pickLevs(array $a, array|null $la = []) {
-		
-		$pa = [];
-		
-		if (isset(   $la[0])) {
-			$a0 = $a[$la[0]];
-			for($i = 0; $i < count($a0); $i++) $pa[$i] = $i;
-			unset($pa[$la[1]]);
-			$pa = array_values($pa);
-			$la[1] = $pa[random_int(0, count($pa) - 1)];
-			return $la;
-				
-		} else {
-			$la[0] = random_int(0, count($a) - 1);
-			$la[1] = random_int(0, count($a[$la[0]]) - 1);
+		$p0a = $this->pickLevs(self::names);
+		$p0nm = self::names[$p0a[0]][$p0a[1]];
+		$ta   = self::names[$p0a[0]];
+		unset($ta[$p0a[1]]);
+		$ta = array_values($ta);
+		$p1a = $this->pickLevs($ta);
+		$p1nm = $ta[$p1a[0]];
+		echo($p0nm . ' ' . $p1nm);
+		return;
 
-		}
-		 
-		return $la;
 	}
-	
 }
 
 new iq1();
