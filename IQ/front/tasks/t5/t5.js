8ad781942ce8354e141702041535344e3f299dfb
kwynncom <buessresume@gmail.com>
2023-04-24 22:55:36 -0400

    no feedback mode should now work for all tasks

--- IQ/front/tasks/t5/t5.js
+++ IQ/front/tasks/t5/t5.js
@@ -4,7 +4,9 @@ class t5inter {
 		let isc = false;
 		if (guess == KWIQT5A) { color = 'green'; isc = true; }
 		await feedback(guess, isc);
-		e.style.backgroundColor = color;
-		setTimeout(() => { document.body.addEventListener('click', () => { location.reload(); }); }, 10);
+		if (FMODE === 'imm') {
+			e.style.backgroundColor = color;
+			setTimeout(() => { document.body.addEventListener('click', () => { location.reload(); }); }, 10);
+		} else location.reload();
 	}
 }
