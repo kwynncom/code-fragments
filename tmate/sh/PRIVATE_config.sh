aa69aef0a79b07a7f054c6b21a31f88a7ab9ed40
kwynncom <buessresume@gmail.com>
2023-07-05 18:34:14 -0400

    changing examples

--- tmate/sh/PRIVATE_config.sh
+++ tmate/sh/PRIVATE_config.sh
@@ -1,3 +1,3 @@
-export URL="https://tl1.knightsofcode.net/t/23/07/tmate/s.php"
+export URL="https://example.com/t/23/07/tmate/s.php"
 
-# echo d | curl -X POST -d "$(</dev/stdin)" https://tl1.knightsofcode.net/t/23/07/tmate/s.php
+# echo d | curl -X POST -d "$(</dev/stdin)" https://example.com/t/23/07/tmate/s.php
