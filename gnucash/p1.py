9a7b24e77d231bf838c8b96883a40e6fea1553e2
Kwynn Buess <buessresume@gmail.com>
2025-02-17 02:03:53 -0500

    finding all transactions with descriptions

--- gnucash/p1.py
+++ gnucash/p1.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
 import sys
-import json
 
 from gnucash import GncNumeric, Session, SessionOpenMode
 
 def find_account_by_name(root_account, name):
-    # Check the current account
     if root_account.name == name:
         return root_account
-
-    # Recursively search through children
     for child in root_account.get_children():
         found = find_account_by_name(child, name)
         if found:
             return found
     return None
 
-
 if __name__ == '__main__':
-    print('Name,Commodity,Totals,Totals (USD)')
     with Session(sys.argv[1], SessionOpenMode.SESSION_READ_ONLY) as session:
-        commodity_table = session.book.get_table()
-        USD = commodity_table.lookup("ISO4217", "USD")
         root_account = session.book.get_root_account()
-        found = find_account_by_name(root_account, 'secret25')
+
+        relevant_aname = 'secret25'
+
+        account = find_account_by_name(root_account, relevant_aname)
+        splits = account.GetSplitList()
+
+        for split in splits:
+            transaction = split.GetParent()
+            trans_splits = transaction.GetSplitList()
+            for trans_split in trans_splits:
+                aname = trans_split.GetAccount().GetName()
+                if aname == relevant_aname:
+                    continue
+
+                print(trans_split.GetValue().to_double(), aname, transaction.GetDescription())
         ignore = 1
 
