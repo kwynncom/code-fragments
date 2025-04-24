4b182ceb35354d31b58de4025fc789cbc9671c2b
Kwynn Buess <buessresume@gmail.com>
2025-04-24 04:00:05 -0400

    whittling on GNUCash

--- gnucash/p1.py
+++ gnucash/p1.py
@@ -10,7 +10,6 @@ def find_account_by_name(root_account, name):
     if root_account.name == name:
         return root_account
     for child in root_account.get_children():
-        ckname = child.name
         found = find_account_by_name(child, name)
         if found:
             return found
@@ -19,19 +18,15 @@ def find_account_by_name(root_account, name):
 if __name__ == '__main__':
     with Session(GNUCASH_FILE_PATH, SessionOpenMode.SESSION_READ_ONLY) as session:
 
-# Remember that GNUCash creates a new XML file with each save, so hard links don't work
-
-        relevant_aname = GNUCASH_ACCT
+        account_to_show = GNUCASH_ACCT
 
         root_account = session.book.get_root_account()
-        account = find_account_by_name(root_account, relevant_aname)
+        account = find_account_by_name(root_account, account_to_show)
         splits = account.GetSplitList()
 
         iacct = 0
-        tot = 0
+        runningBalance = 0
         list = []
-
-        asj = 1 # as JSON
 
         for split in splits:
 
@@ -41,49 +36,33 @@ if __name__ == '__main__':
                 recon = '?'
                 aname = trans_split.GetAccount().GetName()
 
-                if aname == relevant_aname:
-                    continue
-                else :
+                if aname == account_to_show:
+                   continue  # I'm not interested in the credit card side of the split; I want to know where the money went
+                else : # but I do want I want the reconciled flag from the credit card side
                     nlen = len(trans_splits)
                     ir1 =  (1 - i) if nlen == 2 else  0
                     recon = trans_splits[ir1].GetReconcile()
 
                 trans_date = transaction.GetDate()
-                hu = trans_date.strftime('%m/%d')
-                huyr = trans_date.strftime('%m/%d/%Y')
                 f = trans_split.GetValue().to_double()
-                fs =  "{:,.2f}".format(f)
-                tot += f
-                tots = "{:,.2f}".format(tot)
-                descr = transaction.GetDescription()
-                # recon = trans_split.GetReconcile()
-
-                entered_time64 = transaction.GetDateEntered()
+                runningBalance += f
 
                 o = {
+                    'to': aname,
+                    'huDatePosted': trans_date.strftime('%m/%d/%Y'),
+                    'amount': f,
+                    'bal': round(runningBalance, 2),
+                    'descr'  : transaction.GetDescription(),
+                    'reconciled'  : recon,
+                    'i': iacct,
+                    'Uposted'  : int(trans_date.timestamp()),
+                    'Ucreated' : int(transaction.GetDateEntered().timestamp()),
                     'splitGUID': split.GetGUID().to_string(),
-                    'hu' : hu,
-                    'd'  : descr,
-                    'nm' : aname,
-                    'aGUID' : trans_split.GetAccount().GetGUID().to_string(),
-                    'r'  : recon,
-                    'f'  : f,
-                    'bal' : round(tot, 2),
-                    'huyr' : huyr,
-                    'Upost' : int(trans_date.timestamp()),
-                    'i' : iacct,
-                    'Ucre' : int(entered_time64.timestamp()),
-
                 }
 
                 iacct += 1
                 list.append(o)
 
-                if not asj :
-                    print(hu, descr , aname, recon, fs, tots)
-
-        if asj:
-           json.dump(list, sys.stdout, indent=4)
-           print()
-           # print('done')
+    json.dump(list, sys.stdout, indent=4)
+    print()
 
