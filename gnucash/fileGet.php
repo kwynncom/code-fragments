cad2fbdbc2eac6db2fda9fd75e19e0b0646314b5
Kwynn Buess <buessresume@gmail.com>
2025-06-07 20:24:33 -0400

    cache working

--- gnucash/fileGet.php
+++ gnucash/fileGet.php
@@ -7,21 +7,51 @@ class xactsGetCl {
     const pyCmd = 'python3 ' . __DIR__ . '/../pygnucash/main.py';
     const cachePre  = '/var/kwynn/gnucash/cache';
     const cacheSfx = '.json';
+    const xmlf = '/var/kwynn/gnucash/gnucash.xml.gnucash';
 
     public readonly float $balStart;
     public readonly array $currXacts;
+    private	    bool  $iAmCache = false;
 
     public function __construct() {
 	$this->do10();
 	$this->putCache();
     }
 
-    private function putCache() {
+    private function getCacheName() : string {
 	$f  = '';
 	$f .= self::cachePre;
 	$f .= '-';
 	$f .= PHP_SAPI === 'cli' ? 'cli' : 'www';
 	$f .= self::cacheSfx;
+	return $f;
+    }
+
+    private function getCacheTxt() {
+	$this->iAmCache = true;
+ 	return file_get_contents($this->getCacheName());
+    }	
+
+    private function getCurrJSON() : string {
+	$doRaw = false;
+	$cf = $this->getCacheName();
+	if (!file_exists($cf)) $doRaw = true;
+	
+	kwas(file_exists(self::xmlf), 'GNUCash XML does not exist err #201034');
+	$rp = realpath(self::xmlf);
+	
+
+	if (!$doRaw && (filemtime($cf) < filemtime($rp))) $doRaw = true;
+
+	if ($doRaw) return $this->getRaw();
+	else	    return $this->getCacheTxt();
+    }
+
+    private function putCache() {
+
+	if ($this->iAmCache) return;
+
+	$f = $this->getCacheName();
 
 	file_put_contents($f, '');
 	kwas(chmod($f, 0600), "cannot chmod $f");
@@ -33,7 +63,7 @@ class xactsGetCl {
     }
 
     private function do10() {
-	$t = $this->getRaw();
+	$t = $this->getCurrJSON();
 	$this->do20A($t);
 	
     }
@@ -41,7 +71,10 @@ class xactsGetCl {
     private function do20A(string $t) {
 
 	$afwd = json_decode($t, true); unset($t);
-	$a = array_reverse($afwd); unset($afwd);
+	if (!$this->iAmCache) $a = array_reverse($afwd); 
+	else $a = $afwd;
+
+	unset($afwd);
 
 	kwas($a && is_array($a), 'JSON did not yield initial array err# 063933');
 
@@ -56,7 +89,9 @@ class xactsGetCl {
 
 	if (!is_float($startBal)) $startBal = floatval($startBal);
 
-	$a = array_slice($a, 0, $i + 1); unset($i);
+	if (!$this->iAmCache) $a = array_slice($a, 0, $i + 1); unset($i);
+
+	// $a = array_reverse($a);
 	
 	$this->balStart = $startBal; unset($startBal);
 	$this->currXacts = $a;
