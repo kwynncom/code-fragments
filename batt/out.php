1f299fbdcbe8e0447471472b2e23c4d4a6c6ca93
Kwynn Buess <buessresume@gmail.com>
2025-09-22 21:07:52 -0400

    writing to file

--- batt/out.php
+++ batt/out.php
@@ -7,22 +7,53 @@ class adbDisplayCl {
 
     private function do10(array $ain) {
 	$s  = '';
+	$f  = '';
+
 	foreach($ain as $sn => $a) {
+	    $batt = $a['battery'];
 	    $U  = $a['Uat'];
-	    $s .= $a['battery']->level;
+	    $s .= $batt->level;
 	    $s .= '%';
 	    $s .= ' ';
-	    $s .= $a['battery']->chargingBy ? '++' : '--';
+	    $s .= $batt->chargingBy ? '++' : '--';
 	    $s .= ' ';
-	    $s .= date('H:i', $U);
+	    $f  = $s;
+	    $s .= sprintf('%0.3f', $batt->V);
+	    $f .= $batt->V;
+	    $s .= 'V';
+	    $s .= ' '; 
+	    $f .= ' ';
+	    $s .= number_format($batt->uAh) . 'uAh';
+	    $f .= $batt->uAh;
 	    $s .= ' ';
+	    $f .= ' ';
+	    $hu = date('H:i:s D', $U);
+	    $s .= $hu;
+	    $f .= $hu;
+	    $s .= ' ';
+	    $f .= ' ';
 	    $s .= $a['gen']['ro.product.manufacturer'];
+	    $f .= $a['gen']['ro.serialno'];
+	    $f .= ' ';
 	    $s .= ' ';
-	    $s .= $U;
+	    $f .= 'v925-1';
+	    $f .= ' ';
+	    $f .= $U;
+	    $s .= ' ';
+	    $f .= ' ';
 
 	}
 
-	if (iscli()) echo($s . "\n");
+	$f .= "\n";
+	if (iscli()) {
+	    echo($s . "\n");
+	    if (false) echo($f);
+	}
+
+	$fn = '/var/kwynn/batt.txt';
+	$n = file_put_contents($fn, $f, FILE_APPEND);
+	kwas($n === strlen($f), 'bad write to ' . $fn);
+
     }
 
 }
