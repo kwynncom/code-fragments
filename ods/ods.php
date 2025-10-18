14293eb59f9079da053464bc9b79d9c1c2546ac3
Kwynn Buess <buessresume@gmail.com>
2025-10-18 02:43:27 -0400

    my own hours ahead calcs

--- ods/ods.php
+++ ods/ods.php
@@ -4,6 +4,43 @@ require_once('/opt/kwynn/kwutils.php');
 
 class odsFirstSheetCl {
     const source = '/var/kwynn/hours/';
+    const marker = 'autoKw18';
+    const fs = ['marker', 'start', 'hours', 'permonth', 'rate'];
+    const dperm = 30.42;
+    
+
+    private function do40(array $a) {
+	if (!$a) return;
+
+	$now = time();
+
+	// $tz = date_default_timezone_get();
+	$s = $now - strtotime($a[1]); // . ' ' . $tz);
+	$hus = number_format($s);
+	$elapM = ($s) / (self::dperm * DAY_S);
+	$paid  = $elapM * $a[3];
+	$dph   = $paid /  $a[2];
+	$worked = $a[4] * $a[2];
+	$dahead  = $worked - $paid;
+	$hrahead = $dahead / $a[4];
+	$targdpd = $a[3] / self::dperm;
+	$targhpd = $targdpd / $a[4];
+	$daysahead = $dahead / $targdpd;
+	$Uearnto = roint($now + $daysahead * DAY_S);
+	$earntohu = date('r', $Uearnto);
+	
+	return;
+    }
+
+
+    private function findMarker(array $a) : array {
+	foreach($a as $i => $c) {
+	    if (trim($c) !== self::marker) continue;
+	    $ret = array_slice($a, $i);
+	    return $ret;
+	}
+	return [];
+    }
 
     public function __construct() {
 	$this->do10();
@@ -11,12 +48,29 @@ class odsFirstSheetCl {
 
     private function do10() {
 	$fs = glob(self::source . '*.ods');
-	foreach($fs as $f) $this->do20($f);
+	foreach($fs as $f) {
+	    $this->do20 ($f);
+	    $this->parse30($f);
+	}
 	return;
     }
 
+
+
+    private function parse30(string $f) {
+	$csv = $this->otoc($f);
+	$t = file_get_contents($csv);
+	$a = str_getcsv($t);
+	$this->do40($this->findMarker($a));
+	return;
+    }
+
+    private function otoc(string $f) {
+	return str_replace('.ods', '.csv', $f);
+    }
+
     private function already(string $f) : bool {
-	$csv = str_replace('.ods', '.csv', $f);
+	$csv = $this->otoc($f);
 	if (filemtime($csv) >= filemtime($f)) return true;
 	else return false;
     }
@@ -25,7 +79,6 @@ class odsFirstSheetCl {
 
 	if ($this->already($f)) return;
 
-	//    soffice --headless --convert-to csv blah.ods --outdir .
 	$c = 'soffice --headless --convert-to csv ' . $f . ' --outdir ' . self::source;
 	$res = shell_exec($c);
 
