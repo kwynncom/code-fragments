22ac23ff6d0be25210fdcb32733c8da37409e0d2
Kwynn Buess <buessresume@gmail.com>
2025-11-16 01:44:20 -0500

    more whittling

--- Wordle/cli/cmp.php
+++ Wordle/cli/cmp.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+require_once('/opt/kwynn/kwutils.php'); // only forces warnings to fatal errors, so far
+
 define('F',      5 );
 define('U', F -  1 );
 define('Y',     '1');
@@ -8,14 +12,14 @@ define('M',     '+');
 define('P',     '?');
 define('I', str_pad('', F, P));
 
-function cmp(string $a, string $b) : string {
+function cmp(string $s, string $g) : string {
 
     $r = I;
 
     for ($i=0; $i < F; $i++) {	
     for ($j=0; $j < F; $j++) {
 	if ($r[$j] === Y     ) { continue; }
-	if ($b[$j] === $a[$i]) { $r[$j] = $i === $j ? Y : M; continue; }
+	if ($g[$j] === $s[$i]) { $r[$j] = $i === $j ? Y : M; continue; }
 	if ($i < U)	       { continue;   }
 	if ($r[$j] === P)      { $r[$j] = N; }  
     } // for
