32e8ec29d9d8b7204540078af31f03470ff36b2e
Kwynn Buess <buessresume@gmail.com>
2025-11-17 05:10:39 -0500

    unused define() removed

--- Wordle/cli/cmp.php
+++ Wordle/cli/cmp.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 require_once('/opt/kwynn/kwutils.php'); // only forces warnings to fatal errors, so far
 
 define('F',      5 );
-define('U', F -  1 );
 define('Y',     '1');
 define('N',     '0');
 define('M',     '+');
